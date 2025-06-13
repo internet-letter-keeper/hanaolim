eb2523dbf7075732225c652f5ad55465894a8b9a
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2025-06-13 13:36:56 +0900

    Chore: Primary 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,5 @@
 import PrimaryButton from "@/components/atoms/PrimaryButton";
 
 export default function Page() {
-  return (
-    <>
-      홈
-      <div className="flex flex-col gap-4 mx-5">
-        <PrimaryButton
-          title="하나은행"
-          color="green"
-          textSize="lg"
-          weight="cm"
-        />
-        <PrimaryButton
-          title="수빈 업"
-          color="white"
-          textSize="sm"
-          weight="cm"
-        />
-      </div>
-    </>
-  );
+  return <>홈</>;
 }
