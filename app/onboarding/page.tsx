ab77d2e6667512c2f3cbd2d6a7e843d66513e100
ahhyun <ahhyun1106@gmail.com>
2025-06-13 23:52:17 +0900

    Refactor: OnboardingCard 컴포넌트의 추상화 정도를 높임

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,20 +1,10 @@
-import OnboardingOption from "@/components/OnboardingOption";
+import OnboardingCard from "@/components/OnboardingCard";
 
 export default function OnboardingPage() {
   return (
     <div className="flex flex-col my-4 gap-y-4 items-center">
-      <OnboardingOption
-        title={"나를 군인으로 등록하기"}
-        label={"등록하기"}
-        imgSrc={"/images/ic-byeoldol-face.svg"}
-        color={"green"}
-      />
-      <OnboardingOption
-        title={"보고 싶은 군인 추가하기"}
-        label={"추가하기"}
-        imgSrc={"/images/ic-letter.svg"}
-        color={"blue"}
-      />
+      <OnboardingCard mode={"soldier"} />
+      <OnboardingCard mode={"viewer"} />
     </div>
   );
 }
