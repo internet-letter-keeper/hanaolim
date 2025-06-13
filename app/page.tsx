ae4562de4b2909a03532e8f9d6b1ba6371a25c6d
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2025-06-13 09:37:32 +0900

    Chore: eslint설치 및 폴더 이동

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,3 @@
-import Image from "next/image";
-import Text from "./components/atoms/text";
-
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Text weight="bold" size="xl">
-        하나은행 bold
-      </Text>
-      <Text weight="light" size="sm">
-        하나은행 light
-      </Text>
-      <Text weight="cm">하나은행 cm</Text>
-    </div>
-  );
+  return <div>하나은행</div>;
 }
