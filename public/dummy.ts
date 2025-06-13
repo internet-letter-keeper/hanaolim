7d31805de78167882e31c3d7023a31f276a76edd
ryd10227 <76207466+ryd10227@users.noreply.github.com>
2025-06-14 00:16:58 +0900

    Feat: 캐비넷과 친구 관리 화면에 필요한 친구 프로필 원형 컴포넌트 생성, 캐비넷 화면 상단 친구 목록 컴포넌트 생성 (#11)
    * Design: 별돌이 계급별 프로필 아이콘 svg 4종 추가
    
    * Delete: gitkeep 삭제
    
    * Feat: 친구 프로필 원형 아이콘 컴포넌트의 타입 정의
    
    * Feat: 친구 목록 더미 데이터
    
    * Feat: 친구 프로필 원형 아이콘 컴포넌트
    
    * Feat: 캐비넷 상단 친구 목록 컴포넌트
    
    * Design: 별돌이 상병 프로필 아이콘 svg 사이즈 변경

---
+++ public/dummy.ts
@@ -0,0 +1,82 @@
+import { FriendProfile } from "@/types/common/profile";
+
+export const dummyFriends: FriendProfile[] = [
+  {
+    userName: "별코",
+    endDate: new Date("2024-12-31"),
+    code: "ABC123",
+    level: 3,
+  },
+  {
+    userName: "김재윤",
+    endDate: new Date("2025-12-31"),
+    code: "q234rawe",
+    level: 1,
+  },
+  {
+    userName: "김재윤",
+    endDate: new Date("2025-06-13"),
+    code: "4356ytegr",
+    level: 2,
+  },
+  {
+    userName: "김재윤",
+    endDate: new Date("2025-06-15"),
+    code: "567e4yurht",
+    level: 4,
+  },
+  {
+    userName: "김재윤",
+    endDate: new Date("2025-6-31"),
+    code: "47568ueu",
+    level: 1,
+  },
+  {
+    userName: "김재윤",
+    endDate: new Date("2024-12-31"),
+    code: "34wy6tr",
+    level: 3,
+  },
+  {
+    userName: "김재윤",
+    endDate: new Date("2024-12-31"),
+    code: "w35t4er",
+    level: 3,
+  },
+  {
+    userName: "김재윤",
+    endDate: new Date("2024-12-31"),
+    code: "4768uyt",
+    level: 3,
+  },
+  {
+    userName: "김재윤",
+    endDate: new Date("2024-12-31"),
+    code: "2q534raewf",
+    level: 3,
+  },
+  {
+    userName: "김재윤",
+    endDate: new Date("2024-12-31"),
+    code: "5t4esrg",
+    level: 3,
+  },
+  {
+    userName: "김재윤",
+    endDate: new Date("2024-12-31"),
+    code: "707o89tikuyj",
+    level: 3,
+  },
+  {
+    userName: "김재윤",
+    endDate: new Date("2024-12-31"),
+    code: "07p9o8yiu",
+    level: 3,
+  },
+  {
+    userName: "김재윤",
+    endDate: new Date("2024-12-31"),
+    code: "234erdqw",
+    level: 3,
+  },
+];
