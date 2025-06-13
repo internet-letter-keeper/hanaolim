36e5381fc06ee55aec82b89a5d7b5b2728246d8f
ahhyun <ahhyun1106@gmail.com>
2025-06-13 22:55:05 +0900

    Refactor: 불필요한 div 제거

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -3,22 +3,18 @@ import OnboardingOption from "@/components/OnboardingOption";
 export default function OnboardingPage() {
   return (
     <div className="flex flex-col  my-4 gap-y-4 items-center">
-      <div>
-        <OnboardingOption
-          title={"나를 군인으로 등록하기"}
-          label={"등록하기"}
-          imgSrc={"/images/ic-byeoldol-face.svg"}
-          color={"green"}
-        />
-      </div>
-      <div>
-        <OnboardingOption
-          title={"보고 싶은 군인 추가하기"}
-          label={"추가하기"}
-          imgSrc={"/images/ic-letter.svg"}
-          color={"blue"}
-        />
-      </div>
+      <OnboardingOption
+        title={"나를 군인으로 등록하기"}
+        label={"등록하기"}
+        imgSrc={"/images/ic-byeoldol-face.svg"}
+        color={"green"}
+      />
+      <OnboardingOption
+        title={"보고 싶은 군인 추가하기"}
+        label={"추가하기"}
+        imgSrc={"/images/ic-letter.svg"}
+        color={"blue"}
+      />
     </div>
   );
 }
