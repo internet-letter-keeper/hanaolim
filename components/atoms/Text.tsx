8a00f6017762b0b45ff4a04478224581e0879bb6
ahhyun <ahhyun1106@gmail.com>
2025-06-13 10:32:13 +0900

    Feat: Text 컴포넌트 size 수정, align 추가

---
+++ components/atoms/Text.tsx
@@ -0,0 +1,42 @@
+import { PropsWithChildren, ReactNode } from "react";
+import { cn } from "@/lib/utils";
+
+type Props = {
+  children: ReactNode;
+  className?: string;
+  size?: keyof typeof sizeMap;
+  weight?: keyof typeof fontMap;
+  align?: keyof typeof alignMap;
+};
+
+const alignMap = {
+  left: "text-left",
+  right: "text-right",
+  center: "text-center",
+}
+
+const fontMap = {
+  light: "font-[Hana2-Light]",
+  regular: "font-[Hana2-Regular]",
+  medium: "font-[Hana2-Medium]",
+  cm: "font-[Hana2-CM]",
+  bold: "font-[Hana2-Bold]",
+  heavy: "font-[Hana2-Heavy]",
+};
+
+const sizeMap = {
+  xs: "text-xs",
+  sm: "text-sm",
+  md: "text-base",
+  lg: "text-lg",
+  xl: "text-xl",
+};
+export default function Txt(
+  { children, className, size = "sm", weight = "regular", align = "center" }: PropsWithChildren<Props>
+) {
+  return (
+    <span className={cn("text-black-939", fontMap[weight], sizeMap[size], alignMap[align], className)}>
+      {children}
+    </span>
+  );
+}
