5f61bb4c99a792e1779aaea4f9d46afa0617af73
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2025-06-13 13:27:28 +0900

    Feat: 기본 버튼 컴포넌트

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+import PrimaryButton from "@/components/atoms/PrimaryButton";
+
 export default function Page() {
-  return <>홈</>;
+  return (
+    <>
+      홈
+      <div className="flex flex-col gap-4 mx-5">
+        <PrimaryButton
+          title="하나은행"
+          color="green"
+          textSize="lg"
+          weight="cm"
+        />
+        <PrimaryButton
+          title="수빈 업"
+          color="white"
+          textSize="sm"
+          weight="cm"
+        />
+      </div>
+    </>
+  );
 }
