b8fb442302f4307bba5372ba94c8ee962138674c
ryd10227 <ryd10227@gmail.com>
2025-06-14 16:17:49 +0900

    Chore: layout.tsx 안 쓰는 폰트 삭제

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "하나올림",
-  icons: {
-    icon: "/icons/favicon.svg",
-  },
+  icons: { icon: "/icons/favicon.svg" },
 };
 
 export default function RootLayout({
@@ -27,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased [&::-webkit-scrollbar]:hidden`}
+        className={`antialiased [&::-webkit-scrollbar]:hidden`}
       >
         {children}
       </body>
