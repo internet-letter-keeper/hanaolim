e23c8a6b438ddbd5569727a8c5ddfb0721bd3aa2
ahhyun <ahhyun1106@gmail.com>
2025-06-14 13:23:10 +0900

    Feat: PointItem 컴포넌트 생성

--- app/pointHistory/page.tsx
+++ app/pointHistory/page.tsx
@@ -1,3 +1,35 @@
+import PointItem from "@/components/PointItem";
+
+// TODO: dummy data 추후 실제 데이터로 변경
+function createDummyData() {
+  const data = [];
+
+  for (let i = 9; i >= 0; i--) {
+    data.push({
+      description: "하나은행 편지 이벤트",
+      date: new Date(),
+      amount: 100,
+      balance: 100 * (i + 1),
+    });
+  }
+
+  return data;
+}
+
+const dummyData = createDummyData();
+
 export default function PointHistoryPage() {
-  return <>포인트 내역 조회</>;
+  return (
+    <div>
+      {dummyData.map((item, idx) => (
+        <PointItem
+          key={idx}
+          description={item.description}
+          date={item.date}
+          amount={item.amount}
+          balance={item.balance}
+        />
+      ))}
+    </div>
+  );
 }
---
+++ components/PointItem.tsx
@@ -0,0 +1,48 @@
+import Txt from "./atoms/Text";
+
+type Props = {
+  description: string;
+  date: Date;
+  amount: number;
+  balance: number;
+};
+
+//TODO: 추후 TIMESTAMP 형식에 맞게 변경
+function formatDate(date: Date): string {
+  const yyyy = date.getFullYear();
+  const mm = String(date.getMonth() + 1).padStart(2, "0");
+  const dd = String(date.getDate()).padStart(2, "0");
+  const hh = String(date.getHours()).padStart(2, "0");
+  const min = String(date.getMinutes()).padStart(2, "0");
+
+  return `${yyyy}.${mm}.${dd} ${hh}:${min}`;
+}
+
+export default function PointItem({
+  description,
+  date,
+  amount,
+  balance,
+}: Props) {
+  return (
+    <>
+      <div className="flex flex-row justify-between py-[14px] px-7 ">
+        <Txt size={18} weight="cm">
+          {description}
+        </Txt>
+        <div className="flex flex-col items-end">
+          <Txt size={12} weight="light" className=" text-blue-9a0">
+            {formatDate(date)}
+          </Txt>
+          <Txt size={16} weight="bold" className=" mt-[21px] text-green-49d">
+            +{amount.toLocaleString()} 원
+          </Txt>
+          <Txt size={13} weight="cm" className="text-blue-9a0">
+            {balance.toLocaleString()} 원
+          </Txt>
+        </div>
+      </div>
+      <div className="h-[0.8px] bg-gray-ada mx-7" />
+    </>
+  );
+}
