293cf4afa6c72272f40afbd1562ccf5a20574349
ryd10227 <ryd10227@gmail.com>
2025-06-14 16:30:55 +0900

    Refactor: Input atom 컴포넌트 prop 타입 개선

--- components/atoms/Input.tsx
+++ components/atoms/Input.tsx
@@ -1,3 +1,4 @@
+import { HTMLAttributes, InputHTMLAttributes } from "react";
 import { cn } from "@/lib/utils";
 
 type Props = {
@@ -18,7 +19,8 @@ type Props = {
   disabled?: boolean;
   // 자동 포커스 여부
   autoFocus?: boolean;
-};
+} & InputHTMLAttributes<HTMLInputElement> &
+  HTMLAttributes<HTMLTextAreaElement>;
 
 // auth: 로그인, 회원가입용, 글쓰기 input
 // modal: 모달창용 input
