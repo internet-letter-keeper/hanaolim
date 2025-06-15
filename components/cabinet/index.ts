249db6336a02d7e020c74e6e4364684f7395db17
ryd10227 <ryd10227@gmail.com>
2025-06-16 04:35:51 +0900

    Refactor: 친구 목록 컴포넌트와 친구 추가 컴포넌트를 분리, 추가 버튼 클릭 시 모달 띄우기

--- components/cabinet/index.ts
+++ components/cabinet/index.ts
@@ -1,5 +1,6 @@
 export { default as Cabinet } from "./Cabinet";
 export { default as CabinetHeader } from "./CabinetHeader";
 export { default as FriendsList } from "./FriendsList";
+export { default as AddFriendBtn } from "./AddFriendBtn";
 export { default as LetterMoneyButton } from "./LetterMoneyButton";
 export { default as StatusMessage } from "./StatusMessage";
