6aba14be9dc4e7635a47880edd54f09e833dc93e
JungSoEun <sod00402@konkuk.ac.kr>
2025-06-15 23:13:05 +0900

    Feat: Publishing write page (#18)
    * Style: 전역 배경색 변경
    
    * Feat: Input atom 컴포넌트 생성
    
    * Chore: 프리티어 import문 정렬 규칙에 public, types 폴더 추가
    
    * Design: global - 스크롤바 숨기기
    
    * Design: global.css 필요 없는 폰트 속성 삭제
    
    * Design: global.css 중복된 색상 변수 선언 제거 (blue-4e8)
    
    * Remove: gitkeep 제거
    
    * Feat: TextArea 컴포넌트
    
    * Feat: 글쓰기 페이지 퍼블리싱
    
    * Chore: 아이콘 추가
    
    * Chore: layout.tsx 안 쓰는 폰트 삭제
    
    * Refactor: React import 컨벤션 맞추기
    
    * Refactor: 모바일 레이아웃 구조 개선(미디어쿼리, 양옆 배경색 지정) 및 스크롤바 숨김
    
    * Refactor: atom 컴포넌트들의 index.ts 추가
    
    * Refactor: Input atom 컴포넌트 prop 타입 개선
    
    * Chore: png에서 svg 파일로 변경
    
    * Refactor: Input atom 컴포넌트 max-w-sm 스타일 제거
    
    * Feat: IconPicker 컴포넌트
    
    * Feat: 답장이 아닌 편지를 쓸 경우
    
    * Chore: 배경색 수정
    
    * Fix: PrimaryButton 정렬 추가
    
    * Feat: 이미지, 동영상 미리보기 컴포넌트
    
    * Fix: Next Image 경고 오류
    
    * Feat: 파일 미리보기
    
    * Style: 이미지 스타일 깨짐 수정
    
    * Refactor: 글 내용 500자 제한
    
    * Refactor: Icon Id 변경
    
    * Chore: 컴포넌트 index.ts 추가
    
    * Fix: 타입 정의
    
    * Fix: useState Type 추가
    
    * Fix: 디스트럭처링 및 cn 으로 수정
    
    * Chore: 폴더 변경 및 주석 추가
    
    * Refactor: React Import 변경
    
    * Refactor: Button 으로 변환
    
    * Refactor: 글자색 기본 값 변경에 따른 수정
    
    * Chore: 워딩 변경
    
    * Refactor: 글자 제한 추가
    
    * Refactor: MediaStyle 로 변경
    
    * Refactor: 코드 중복 제거
    
    - 폴더 변경에 따른 import 수정
    - primary Props 수정
    
    * Refactor: PrimaryButton
    
    * Style: cursor-pointer추가
    
    * Refactor: ICONS 상수 분리
    
    * Refactor: 삼항연산자 제거
    
    * Refactor: Props Type 분리
    
    ---------
    
    Co-authored-by: Ausdauer1 <steady051@naver.com>
    Co-authored-by: ryd10227 <ryd10227@gmail.com>

---
+++ components/letters/IconPicker.tsx
@@ -0,0 +1,40 @@
+import { ICONS } from "@/constants/icons";
+import clsx from "clsx";
+import Image from "next/image";
+import { IconName } from "@/types/common/icons";
+import { Button } from "../atoms";
+
+type Props = {
+  value: IconName;
+  onChange: (name: IconName) => void;
+};
+
+export default function IconPicker({ value, onChange }: Props) {
+  return (
+    <div className="flex items-center gap-1">
+      {ICONS.map(({ id, name, src }) => (
+        <Button
+          key={id}
+          type="button"
+          className={clsx(
+            "w-12 aspect-square flex items-center justify-center rounded-full cursor-pointer transition-all duration-200",
+
+            {
+              "bg-white-fff shadow-[0_0_10px_0_rgba(32,155,152,0.35)]":
+                value === name,
+            }
+          )}
+          onClick={() => onChange(name)}
+        >
+          <Image
+            src={src}
+            alt={name}
+            width={33}
+            height={33}
+            className="object-contain w-[33px] h-[33px]"
+          />
+        </Button>
+      ))}
+    </div>
+  );
+}
