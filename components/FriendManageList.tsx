880566acf2066f4d8bcf61d5ca9a1a56d82656f4
ahhyun <ahhyun1106@gmail.com>
2025-06-15 21:48:59 +0900

    Style: boolean props 간소화 및 불필요한 공백 제거

--- components/FriendManageList.tsx
+++ components/FriendManageList.tsx
@@ -9,9 +9,9 @@ export default function FriendManageList() {
         <div className="-mx-4  bg-white-fff">
           <div
             key={profile.code}
-            className="flex flex-row items-center justify-between pt-[12px] pb-[20px] px-7 "
+            className="flex flex-row items-center justify-between pt-[12px] pb-[20px] px-7"
           >
-            <FriendProfileCircle isRowLayout={true} profile={profile} />
+            <FriendProfileCircle isRowLayout profile={profile} />
             <button className="cursor-pointer border-[1px] border-gray-353 px-[17px] rounded-[5px] mt-[8px]">
               <Txt weight="medium">삭제</Txt>
             </button>
