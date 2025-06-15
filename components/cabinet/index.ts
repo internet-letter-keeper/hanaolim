974f4db8da9133da3e62d24f18b2790e5b5cfb67
ryd10227 <ryd10227@gmail.com>
2025-06-16 02:18:07 +0900

    Chore: /cabinet에서 쓰이는 컴포넌트들의 index.ts 추가

---
+++ components/cabinet/index.ts
@@ -0,0 +1,5 @@
+export { default as Cabinet } from "./Cabinet";
+export { default as CabinetHeader } from "./CabinetHeader";
+export { default as FriendsList } from "./FriendsList";
+export { default as LetterMoneyButton } from "./LetterMoneyButton";
+export { default as StatusMessage } from "./StatusMessage";
