091cbe15049134253119c965ec52b4c6ad508426
ahhyun <ahhyun1106@gmail.com>
2025-06-15 21:14:45 +0900

    Style: 마진 조정, border 1px로 수정

--- components/FriendManageList.tsx
+++ components/FriendManageList.tsx
@@ -6,14 +6,17 @@ export default function FriendManageList() {
   return (
     <div>
       {dummyFriends.map((profile) => (
-        <div
-          key={profile.code}
-          className="flex flex-row items-center justify-between pt-[12px] pb-[20px] px-7 border-b bg-white-fff border-gray-ada"
-        >
-          <FriendProfileCircle isRowLayout={true} profile={profile} />
-          <button className="cursor-pointer border-[1px] border-gray-353 px-[17px] rounded-[5px] mt-[8px]">
-            <Txt weight="medium">삭제</Txt>
-          </button>
+        <div className="-mx-4  bg-white-fff">
+          <div
+            key={profile.code}
+            className="flex flex-row items-center justify-between pt-[12px] pb-[20px] px-7 "
+          >
+            <FriendProfileCircle isRowLayout={true} profile={profile} />
+            <button className="cursor-pointer border-[1px] border-gray-353 px-[17px] rounded-[5px] mt-[8px]">
+              <Txt weight="medium">삭제</Txt>
+            </button>
+          </div>
+          <div className="h-[1px] bg-gray-ada mx-7" />
         </div>
       ))}
     </div>
