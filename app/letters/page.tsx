23fc52b194e11f4132d598dc1ce377a65eb0ee3c
cheskt <134696892+cheskt@users.noreply.github.com>
2025-06-15 12:49:26 +0900

    Feat: 편지 목록 컴포넌트 생성 (#16)
    * 기본 헤더 파일(흰색할거면 수정해야해용)
    
    * Feat: 기본 헤더 타이틀 가운데 정렬렬
    
    * Refactor: 기본 헤더 컴포넌트의 타이틀 속성값 옵셔널로 변경
    
    title 속성을 옵셔널로 변경
    
    Co-authored-by: ryd10227 <76207466+ryd10227@users.noreply.github.com>
    
    * Feat: 편지 목록 헤더 부분 생성성
    
    * Feat: 편지 목록 컴포넌트 생성성
    
    ---------
    
    Co-authored-by: ryd10227 <76207466+ryd10227@users.noreply.github.com>

--- app/letters/page.tsx
+++ app/letters/page.tsx
@@ -1,3 +1,54 @@
+"use client";
+
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import Button from "@/components/atoms/Button";
+import Input from "@/components/atoms/Input";
+import { fontMap } from "@/components/atoms/Text";
+import BasicHeader from "@/components/common/BasicHeader";
+
 export default function LettersPage() {
-  return <>편지목록</>;
+  const [activeTab, setActiveTab] = useState<"send" | "receive">("send");
+
+  return (
+    <>
+      <BasicHeader title="편지 보관함" />
+
+      <div className={cn("bg-white-7f9 py-4", fontMap.cm)}>
+        <div className="flex justify-center gap-2">
+          <Button
+            className={cn(
+              "flex justify-center items-center w-[170px] h-[50px] py-[14px] px-0 shrink-0 rounded-lg text-sm",
+              activeTab === "send"
+                ? "bg-green-49d text-white"
+                : "bg-white-2f2 text-green-49d"
+            )}
+            onClick={() => setActiveTab("send")}
+          >
+            내 관물대로 퐁당
+          </Button>
+
+          <Button
+            className={cn(
+              "flex justify-center items-center w-[170px] h-[50px] py-[14px] px-0 shrink-0 rounded-lg text-sm",
+              activeTab === "receive"
+                ? "bg-green-49d text-white"
+                : "bg-white-2f2 text-green-49d"
+            )}
+            onClick={() => setActiveTab("receive")}
+          >
+            친구 관물대
+          </Button>
+        </div>
+
+        <div className="flex justify-center mt-4">
+          <Input
+            placeholder="작성자, 내용 ..."
+            inputType="search"
+            className="w-[336px]"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
