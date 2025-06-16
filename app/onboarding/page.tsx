5b52fb645fe868a8d9ed90547b088138414e6209
ahhyun <ahhyun1106@gmail.com>
2025-06-16 10:17:09 +0900

    Feat: 온보딩 화면 퍼블리싱

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
 import OnboardingCard from "@/components/OnboardingCard";
 
 export default function OnboardingPage() {
   return (
-    <div className="flex flex-col my-4 gap-y-4 items-center">
-      <OnboardingCard mode={"soldier"} />
+    <div className="flex flex-col mt-[193px] my-4 gap-y-5 items-center">
+      <Image
+        src={"/images/ic-hanaolim-logo.svg"}
+        alt={"군장병을 위한 서비스 하나올림의 로고"}
+        width={122}
+        height={40}
+      />
+      <OnboardingCard mode={"soldier"} className="mt-16" />
       <OnboardingCard mode={"viewer"} />
     </div>
   );
