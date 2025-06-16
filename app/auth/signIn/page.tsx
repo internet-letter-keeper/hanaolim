14ebc728df8dcaa2b69576cd47c3a644d8fa8008
Ausdauer1 <steady051@naver.com>
2025-06-16 11:32:48 +0900

    Refactor: 로그인 페이지 반응형으로 수정

--- app/auth/signIn/page.tsx
+++ app/auth/signIn/page.tsx
@@ -7,55 +7,48 @@ import hanaOlim from "@/public/images/ic-hanaolim-logo.svg";
 
 export default function SignInPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center gap-1 h-full px-[20px]">
       {/* 하나 올림 로고 */}
-      <div className="relative w-[184px] h-[49px]">
-        <Image
-          src={hanaOlim}
-          alt="Hana Olim"
-          className="w-full h-full object-cover"
-        />
-      </div>
+      <Image src={hanaOlim} alt="하나올림 로고" width={184} height={49} />
+
       {/* 통합 로그인 텍스트 */}
-      <Txt
-        weight="cm"
-        align="center"
-        className="text-[#AAAAAA] not-italic font-normal leading-none mt-1"
-      >
+      <Txt weight="cm" align="center" className="text-gray-aaa mt-1">
         통합 로그인
       </Txt>
-      {/* 아이디, 비밀번호 입력 필드 + 로그인 버튼 + 또는 */}
-      <div className="mt-[44px] w-[249px]">
-        {/* 아이디 */}
-        <div className="flex justify-between">
-          <Txt
-            size={19}
-            weight="cm"
-            className="text-[#393939] not-italic font-normal leading-none mt-[7px]"
-          >
-            아이디
+
+      {/* 이메일, 비밀번호 입력 필드 + 로그인 버튼 + 또는 */}
+      <div className="mt-[44px] flex flex-col ">
+        {/* 이메일 */}
+        <div className="flex justify-between items-center">
+          <Txt size={19} weight="cm" align="left" className="min-w-1/4">
+            이메일
           </Txt>
           <Input
-            placeholder=""
+            placeholder="이메일을 입력해주세요"
             inputType="auth"
-            className="flex w-[170px] items-center"
+            maxLength={30}
           />
         </div>
+
         {/* 비밀번호 */}
-        <div className="flex mt-4 justify-between">
-          <Txt
-            size={19}
-            weight="cm"
-            className="text-[#393939] not-italic font-normal leading-none mt-[7px]"
-          >
+        <div className="flex mt-4 justify-between items-center mb-[21px]">
+          <Txt size={19} weight="cm" align="left" className="min-w-1/4 ">
             비밀번호
           </Txt>
           <Input
-            placeholder=""
+            placeholder="비밀번호를 입력해주세요"
             inputType="auth"
-            className="flex w-[170px] items-center"
+            maxLength={20}
           />
         </div>
+
+        {/* 로그인 에러 메시지 */}
+        {isLoginError && (
+          <Txt size={12} align="center" className="text-red-a76 mb-[15px]">
+            이메일 또는 비밀번호를 확인해주세요
+          </Txt>
+        )}
+
         {/* 로그인 버튼 */}
         <PrimaryButton
           title="로그인"
@@ -63,32 +56,29 @@ export default function SignInPage() {
           textSize={16}
           align="center"
           weight="cm"
-          className="mt-[22px] h-[38px] font-normal"
-        ></PrimaryButton>
+          className="h-[38px] "
+        />
+
         {/* 또는 */}
-        <div className="flex items-center justify-center mt-[16px] w-full h-[21px]">
-          <svg className="mr-2" width="102" height="1">
+        <div className="flex items-center gap-2 mt-[16px] w-full h-[21px]">
+          <svg width="100%" height="1">
             <line
               x1="0"
               y1="0.5"
-              x2="102"
+              x2="100%"
               y2="0.5"
               stroke="var(--color-gray-fdf)"
               strokeWidth="1"
             />
           </svg>
-          <Txt
-            size={13}
-            weight="cm"
-            className="text-[#AAAAAA] not-italic font-normal leading-[21px] text-center"
-          >
+          <Txt size={13} weight="medium" className="text-gray-aaa w-1/2">
             또는
           </Txt>
-          <svg className="ml-2" width="102" height="1">
+          <svg width="100%" height="1">
             <line
               x1="0"
               y1="0.5"
-              x2="102"
+              x2="100%"
               y2="0.5"
               stroke="var(--color-gray-fdf)"
               strokeWidth="1"
@@ -96,54 +86,46 @@ export default function SignInPage() {
           </svg>
         </div>
       </div>
+
       {/* 소셜 로그인 */}
-      <div className="flex mt-[17px] w-[138px]">
+      <div className="flex items-center mt-[17px] gap-4">
         <button>
-          <Image className="w-[38px] h-[38px]" alt="naverImage" src={icNaver} />
+          <div className="w-[45px] h-[45px]">
+            <Image className="w-full h-full" alt="naverImage" src={icNaver} />
+          </div>
         </button>
-        <button className="ml-[12px]">
-          <Image
-            className="w-[37.909px] h-[38px] flex-shrink-0 aspect-[37.91/38] rounded-[100px]"
-            alt="kakaoImage"
-            src={icKakao}
-          />
+
+        <button>
+          <div className="w-[45px] h-[45px]">
+            <Image className="w-full h-full" alt="kakaoImage" src={icKakao} />
+          </div>
         </button>
-        <button className="ml-[12px]">
-          <div className="w-[36.828px] h-[38px] flex-shrink-0 bg-white rounded-[18.41px/19px]">
+
+        <button>
+          <div className="w-[45px] h-[45px] bg-white rounded-full flex items-center justify-center">
             <Image
-              className="relative w-[26.192px] h-[26.192px] flex-shrink-0 aspect-[26.19/26.19] top-1.5 left-[5px]"
+              className="w-[28px] h-[28px]"
               alt="googleImage"
               src={icGoogle}
             />
           </div>
         </button>
       </div>
+
       {/* 회원가입 안내 */}
-      <div
-        className="flex items-center
-       mt-[45px]"
-      >
-        <Txt
-          size={12}
-          className="not-italic font-normal leading-none text-[#535353]"
-        >
+      <div className="flex items-center mt-[45px]">
+        <Txt size={12} className="not-italic  text-gray-353">
           가입한 계정이 없으신가요?
         </Txt>
         <button className="ml-[14px] mb-1">
           <Txt
             size={12}
-            className="not-italic font-normal leading-none text-[#535353] underline underline-offset-auto"
+            className="not-italic  text-gray-353 underline underline-offset-auto"
           >
             회원가입
           </Txt>
         </button>
       </div>
-      {/* 최상단 div 끝 */}
     </div>
   );
 }
-
-// taildwind 설명
-// font-style: normal;	not-italic
-// font-weight: 400;	font-normal
-// line-height: 21px;	leading-[21px] // line-height가 normal이면 leading-none
