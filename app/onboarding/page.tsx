6a7b179a8021365f10bbaef97bd01f5124d54bee
ahhyun <ahhyun1106@gmail.com>
2025-06-16 17:41:52 +0900

    Feat: 온보딩 페이지 반응형으로 변경

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -3,7 +3,7 @@ import OnboardingCard from "@/components/OnboardingCard";
 
 export default function OnboardingPage() {
   return (
-    <div className="h-full flex flex-col gap-y-5 items-center justify-center ">
+    <div className="h-full flex flex-col gap-y-5 px-6 items-center justify-center pb-20">
       <Image
         src={"/images/ic-hanaolim-logo.svg"}
         alt={"군장병을 위한 서비스 하나올림의 로고"}
--- components/OnboardingCard.tsx
+++ components/OnboardingCard.tsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
 import { ONBOARDING_MODE } from "@/constants/onboardingMode";
 import { cn } from "@/lib/utils";
-import Button from "./atoms/Button";
-import Txt from "./atoms/Text";
+import { Button, Txt } from "./atoms";
 
 //onBording 컴포넌트에 들어갈 option 컴포넌트
 
@@ -19,17 +18,17 @@ export default function OnboardingOption({ mode }: Props) {
   return (
     <Button
       className={cn(
-        "px-[300px] h-[130px] rounded-[20px] p-[18px] border bg-green-5f2 border-green-9e7",
+        "h-[130px] rounded-[20px] p-[18px] border bg-green-5f2 border-green-9e7",
         { "bg-blue-0f5 border-blue-af0": mode === "viewer" }
       )}
     >
-      <div className="flex flex-row gap-[9px]">
-        <div className="flex flex-col gap-y-[11px] pl-1">
+      <div className="flex gap-[9px] justify-between">
+        <div className="flex flex-col gap-y-[11px]">
           <Txt size={18} align="left" weight="bold">
             {title}
           </Txt>
-          <div className="flex flex-row">
-            <Txt size={12} weight="cm" className="text-gray-353 pr-2">
+          <div className="flex gap-2">
+            <Txt size={12} weight="cm" className="text-gray-353">
               {label}
             </Txt>
             <Image
@@ -40,9 +39,7 @@ export default function OnboardingOption({ mode }: Props) {
             />
           </div>
         </div>
-        <div>
-          <Image src={imgSrc} alt={imgAlt} width={70} height={70} />
-        </div>
+        <Image src={imgSrc} alt={imgAlt} width={70} height={70} />
       </div>
     </Button>
   );
