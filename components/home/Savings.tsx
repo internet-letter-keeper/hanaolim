b6e9b43c63aae8e62dd0d104a5ed746ab35be325
soobin <subin3620@naver.com>
2025-06-16 11:15:53 +0900

    Feat: 홈 적금 컴포넌트 생성

--- components/home/Savings.tsx
+++ components/home/Savings.tsx
@@ -1,3 +1,69 @@
+"use client";
+
+import Image from "next/image";
+import Txt from "@/components/atoms/Text";
+
 export default function Savings() {
-  return <>하나 장병내일준비 적금</>;
+  return (
+    <div
+      className="flex flex-col justify-between px-6 py-4 bg-white-fff rounded-[20px] w-max h-[153px] shadow-sm"
+      style={{ boxShadow: "0px 0px 5px 0px rgba(0, 0, 0, 0.15)" }}
+    >
+      {/* 적금 제목, 금액, 이미지 */}
+      <div className="flex justify-between w-full">
+        <div className="flex flex-col items-start gap-1">
+          <Txt size={16} weight="bold" className="text-gray-939">
+            하나 장병내일준비 적금
+          </Txt>
+          <Txt size={16} weight="bold" className="text-gray-939">
+            2,109,762원
+          </Txt>
+        </div>
+        <div className="flex-shrink-0 relative w-[80px] h-[60px] ml-15">
+          <Image
+            src="/images/ic-bankbook.svg"
+            alt="적금 통장"
+            fill
+            className="object-contain"
+            priority
+          />
+        </div>
+      </div>
+
+      {/* 조건 달성 여부 */}
+      <div className="flex flex-col gap-1 mt-2 w-full">
+        <div className="flex items-center w-full">
+          <div className="flex items-center justify-center w-[50px] h-[20px] bg-green-49d rounded-[10px] mr-2">
+            <Txt size={12} weight="medium" className="text-white-2f2">
+              달성
+            </Txt>
+          </div>
+          <Txt size={12} weight="medium" className="text-gray-939">
+            주택청약종합저축
+          </Txt>
+          <div className="ml-auto">
+            <Txt size={10} weight="medium" className="text-gray-939">
+              연 0.50%
+            </Txt>
+          </div>
+        </div>
+
+        <div className="flex items-center w-full">
+          <div className="flex items-center justify-center w-[50px] h-[20px] bg-white-2f2 rounded-[10px] mr-2">
+            <Txt size={12} weight="medium" className="text-green-49d">
+              미달성
+            </Txt>
+          </div>
+          <Txt size={12} weight="medium" className="text-gray-939">
+            하나카드결제
+          </Txt>
+          <div className="ml-auto">
+            <Txt size={10} weight="medium" className="text-gray-939">
+              연 0.70%
+            </Txt>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
