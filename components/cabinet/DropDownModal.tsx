d9003031fa50d4349d8f8ca65571f3cf10f5a8bc
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2025-06-16 14:59:16 +0900

    Feat: DropDownModal

--- components/cabinet/DropDownModal.tsx
+++ components/cabinet/DropDownModal.tsx
@@ -10,41 +10,44 @@ import Profile from "../common/Profile";
 const ButtonStyle = "border border-gray-353/25 py-2";
 
 export default function DropDownModal() {
+  // 세션 프로바이더로 나중에 받아와서 구분하기
   return (
-    <Sidebar>
-      <SidebarContent>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <div className="flex flex-col m-7 mt-14 gap-6">
-              <Profile />
-              <div className="flex flex-col gap-2">
-                <PrimaryButton
-                  title="편지 보관함"
-                  className={ButtonStyle}
-                  textSize={14}
-                  weight="regular"
-                  color="gray"
-                />
-                <div className="flex gap-2">
+    <div className="flex justify-center items-center">
+      <Sidebar>
+        <SidebarContent>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <div className="flex flex-col m-7 mt-14 gap-6">
+                <Profile userName="별돌이" endDate={new Date()} />
+                <div className="flex flex-col gap-2">
                   <PrimaryButton
-                    title="군인으로 등록하기"
+                    title="편지 보관함"
                     className={ButtonStyle}
+                    textSize={14}
+                    weight="regular"
                     color="gray"
                   />
-                  <PrimaryButton
-                    title="친구 관리"
-                    className={ButtonStyle}
-                    color="gray"
-                  />
+                  <div className="flex gap-2">
+                    <PrimaryButton
+                      title="군인으로 등록하기"
+                      className={ButtonStyle}
+                      color="gray"
+                    />
+                    <PrimaryButton
+                      title="친구 관리"
+                      className={ButtonStyle}
+                      color="gray"
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="flex justify-center">
-              <div className="bg-gray-353/25 h-1 rounded-[2px] mb-[6px] w-1/5 " />
-            </div>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarContent>
-    </Sidebar>
+              <div className="flex justify-center">
+                <div className="bg-gray-353/25 h-1 rounded-[2px] mb-[6px] w-1/5 " />
+              </div>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarContent>
+      </Sidebar>
+    </div>
   );
 }
--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -5,7 +5,6 @@ import { Slot } from "@radix-ui/react-slot";
 import { cva, VariantProps } from "class-variance-authority";
 import { PanelLeftIcon } from "lucide-react";
 import * as React from "react";
-import { useState } from "react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
