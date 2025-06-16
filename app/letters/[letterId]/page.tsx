d4e8b188282a83c542623ca73ac7dfcb3099381a
cheskt <andy9793@naver.com>
2025-06-16 15:27:02 +0900

    Feat: 편지 조회 상세 페이지 생성성

--- app/letters/[letterId]/page.tsx
+++ app/letters/[letterId]/page.tsx
@@ -1,3 +1,37 @@
-export default function LetterDetailPage() {
-  return <>편지 상세 조회</>;
+import { notFound } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { PrimaryButton } from "@/components/atoms";
+import { fontMap } from "@/components/atoms/Text";
+import BasicHeader from "@/components/common/BasicHeader";
+import LettersDetail from "@/components/letters/LettersDetail";
+import { dummyLetters } from "@/public/dummyLetters";
+
+type Props = {
+  params: {
+    letterId: string;
+  };
+};
+
+export default function LetterDetailPage({ params }: Props) {
+  const letterId = parseInt(params.letterId);
+  const letter = dummyLetters.find((l) => l.id === letterId);
+
+  if (!letter) return notFound();
+
+  return (
+    <>
+      <div className="w-full" />
+      <BasicHeader />
+      {/* 여기서부터 편지 상세 페이지 */}
+
+      <div className={cn("py-4", fontMap.cm)}>
+        <LettersDetail lettersDetail={letter} />
+      </div>
+
+      {/* 답장 버튼 */}
+      <div className="flex justify-end text-center w-full">
+        <PrimaryButton title="답장하기" className="px-3 w-1/4 py-[5px]" />
+      </div>
+    </>
+  );
 }
