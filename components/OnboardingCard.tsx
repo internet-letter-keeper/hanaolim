2476d6b3ea3c947c67e7f2549d8ef1bc7fe8f354
ahhyun <ahhyun1106@gmail.com>
2025-06-16 09:28:10 +0900

    Refactor: OnboardingCard에 있던 modeMap 상수 분리

--- components/OnboardingCard.tsx
+++ components/OnboardingCard.tsx
@@ -1,3 +1,4 @@
+import { modeMap } from "@/constants/onboardingMode";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 import Button from "./atoms/Button";
@@ -7,23 +8,6 @@ import Txt from "./atoms/Text";
 
 type Props = {
   mode: "soldier" | "viewer";
-};
-
-const modeMap = {
-  soldier: {
-    title: "나를 군인으로 등록하기",
-    label: "등록하기",
-    imgSrc: "/images/ic-byeoldol-face.svg",
-    iconAlt: "사용자를 군인으로 등록하는 페이지로 이동하는 버튼",
-    imgAlt: "가운데 별이 박혀 있는 군모를 쓴 하나은행 마스코트 별돌이",
-  },
-  viewer: {
-    title: "보고 싶은 군인 추가하기",
-    label: "추가하기",
-    imgSrc: "/images/ic-letter.svg",
-    iconAlt: "보고 싶은 군인을 추가할 수 있는 페이지로 이동하는 버튼",
-    imgAlt: "편지 모양 아이콘",
-  },
 };
 
 export default function OnboardingOption({ mode }: Props) {
