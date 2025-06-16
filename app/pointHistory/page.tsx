7b3fce748b7a787216b2525a900d6286e3f7273c
ahhyun <ahhyun1106@gmail.com>
2025-06-16 15:11:00 +0900

    Design: 내역 수가 적을 때 하단에 흰색이 아니라 기본 배경색 보이는 문제 해결

--- app/pointHistory/page.tsx
+++ app/pointHistory/page.tsx
@@ -8,13 +8,14 @@ const dummyData = createDummyData();
 
 export default function PointHistoryPage() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col min-h-screen">
       <BasicHeader title="포인트 내역 조회" />
-      <Txt size={25} weight="bold" align="center" className="my-14">
+      <div className="h-[20px]" />
+      <Txt size={25} weight="bold" align="center" className="py-14">
         {/* 포인트 잔액 */}
         {dummyData[0].balance.toLocaleString()} 원
       </Txt>
-      <div className="flex flex-col bg-white-fff h-screen -mx-4">
+      <div className="flex-1 flex flex-col bg-white-fff -m-4">
         <div>
           {/* 잔액/내역 구분선 */}
           <div className="h-[1px] bg-gray-530 mb-4 mt-7 mx-7" />
