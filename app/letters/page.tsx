e20f1d7c8cfdc6d206bc7e29fd1fda1bff73529f
cheskt <andy9793@naver.com>
2025-06-16 15:25:58 +0900

    Refactor: 편지 목록 조회 페이지 컴포넌트 분리 후 수정정

--- app/letters/page.tsx
+++ app/letters/page.tsx
@@ -2,16 +2,25 @@
 
 import { useState } from "react";
 import { cn } from "@/lib/utils";
-import Button from "@/components/atoms/Button";
+import { PrimaryButton } from "@/components/atoms";
 import Input from "@/components/atoms/Input";
-import { fontMap } from "@/components/atoms/Text";
+import Txt, { fontMap } from "@/components/atoms/Text";
 import BasicHeader from "@/components/common/BasicHeader";
-import LettersList from "@/components/letters/LettersList";
+import LettersItem from "@/components/letters/LettersItem";
 import { dummyLetters } from "@/public/dummyLetters";
 
 export default function LettersPage() {
   const [activeTab, setActiveTab] = useState<"send" | "receive">("send");
   const [filter, setFilter] = useState<"all" | "favorite">("all");
+
+  const totalCount = dummyLetters.length;
+  const favoriteCount = dummyLetters.filter(
+    (letter) => letter.isFavorite
+  ).length;
+
+  const onChangeFilter = (value: "all" | "favorite") => {
+    setFilter(value);
+  };
 
   const filteredLetters =
     filter === "all"
@@ -19,56 +28,81 @@ export default function LettersPage() {
       : dummyLetters.filter((letter) => letter.isFavorite);
 
   return (
-    <div className="w-full overflow-x-hidden p-0">
-      <div className="w-full bg-white-7f9">
-        <div className="max-w-screen-sm mx-auto px-4">
-          <BasicHeader title="편지 보관함" />
+    <div className="flex flex-col min-h-screen w-full">
+      <div className="max-w-screen-sm px-2 mb-4">
+        <BasicHeader title="편지 보관함" />
 
-          <div className={cn("py-4", fontMap.cm)}>
-            <div className="flex justify-center gap-2">
-              <Button
-                className={cn(
-                  "flex justify-center items-center w-[170px] h-[50px] py-[14px] px-0 shrink-0 rounded-lg text-sm",
-                  activeTab === "send"
-                    ? "bg-green-49d text-white"
-                    : "bg-white-2f2 text-green-49d"
-                )}
-                onClick={() => setActiveTab("send")}
-              >
-                내 관물대로 퐁당
-              </Button>
+        <div className={cn("py-4", fontMap.cm)}>
+          <div className="flex justify-center gap-2">
+            <PrimaryButton
+              title="내 관물대로 퐁당"
+              onClick={() => setActiveTab("send")}
+              color={activeTab === "send" ? "green" : "white"}
+              className="py-3"
+            />
 
-              <Button
-                className={cn(
-                  "flex justify-center items-center w-[170px] h-[50px] py-[14px] px-0 shrink-0 rounded-lg text-sm",
-                  activeTab === "receive"
-                    ? "bg-green-49d text-white"
-                    : "bg-white-2f2 text-green-49d"
-                )}
-                onClick={() => setActiveTab("receive")}
-              >
-                친구 관물대
-              </Button>
-            </div>
+            <PrimaryButton
+              title="친구 관물대"
+              onClick={() => setActiveTab("receive")}
+              color={activeTab === "receive" ? "green" : "white"}
+              className="py-3"
+            />
+          </div>
 
-            <div className="flex justify-center mt-4">
-              <Input
-                placeholder="작성자, 내용 ..."
-                inputType="search"
-                className="w-[336px]"
-              />
-            </div>
+          <div className="flex justify-center mt-4">
+            <Input placeholder="작성자, 내용 ..." inputType="search" />
           </div>
         </div>
       </div>
 
-      <div className="max-w-screen-sm mx-auto px-4">
-        <LettersList
-          letters={filteredLetters}
-          totalCount={dummyLetters.length}
-          filter={filter}
-          onChangeFilter={setFilter}
-        />
+      <div className="flex-1 py-4 bg-white h-screen -m-4">
+        <div className="flex justify-between items-center px-4 mb-2">
+          <Txt weight="cm" size={13}>
+            총&nbsp;
+            <Txt>
+              {cn(filter === "all" ? `${totalCount}` : `${favoriteCount}`)}
+            </Txt>
+            개
+          </Txt>
+          <div className="flex gap-3">
+            <Txt
+              weight="cm"
+              size={13}
+              className={cn(
+                filter === "all" ? "text-[#209B98]" : "text-black-939",
+                "cursor-pointer underline"
+              )}
+              onClick={() => onChangeFilter("all")}
+            >
+              전체
+            </Txt>
+
+            <Txt weight="cm" size={13}>
+              |
+            </Txt>
+
+            <Txt
+              weight="cm"
+              size={13}
+              className={cn(
+                filter === "favorite" ? "text-green-49d" : "text-black-939",
+                "cursor-pointer underline"
+              )}
+              onClick={() => onChangeFilter("favorite")}
+            >
+              즐겨찾는 편지
+            </Txt>
+          </div>
+        </div>
+        <hr className=" border-gray-939 mx-4" />
+
+        <div className="mx-auto px-4">
+          {filteredLetters.map((letter) => (
+            <LettersItem key={letter.id} letters={letter} />
+          ))}
+        </div>
+
+        {/* <div className="h-16" /> */}
       </div>
     </div>
   );
