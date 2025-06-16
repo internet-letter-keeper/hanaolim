032e9ce926bd5e921dd0f699e98d8e59711b30ca
ahhyun <ahhyun1106@gmail.com>
2025-06-16 15:09:53 +0900

    Refactor: border 상위 div로 옮김

--- components/PointItem.tsx
+++ components/PointItem.tsx
@@ -19,25 +19,22 @@ function formatDate(date: Date): string {
 export default function PointItem({ item }: Props) {
   const { date, amount, balance } = item;
   return (
-    <div className="py-[14px] px-7">
-      <div>
-        <div className="flex flex-row justify-between">
-          <Txt size={18} weight="cm">
-            하나은행 편지 이벤트
+    <div className="pt-[10px] px-7">
+      <div className="flex flex-row justify-between border-b border-gray-ada">
+        <Txt size={18} weight="cm">
+          하나은행 편지 이벤트
+        </Txt>
+        <div className="flex flex-col items-end">
+          <Txt size={12} weight="light" className=" text-blue-9a0">
+            {formatDate(date)}
           </Txt>
-          <div className="flex flex-col items-end">
-            <Txt size={12} weight="light" className=" text-blue-9a0">
-              {formatDate(date)}
-            </Txt>
-            <Txt size={16} weight="bold" className=" mt-[21px] text-green-49d">
-              +{amount.toLocaleString()} 원
-            </Txt>
-            <Txt size={13} weight="cm" className="mb-3.5 text-blue-9a0">
-              {balance.toLocaleString()} 원
-            </Txt>
-          </div>
+          <Txt size={16} weight="bold" className=" mt-[21px] text-green-49d">
+            +{amount.toLocaleString()} 원
+          </Txt>
+          <Txt size={13} weight="cm" className="mb-3.5 text-blue-9a0">
+            {balance.toLocaleString()} 원
+          </Txt>
         </div>
-        <div className="h-[1px] bg-gray-ada" />
       </div>
     </div>
   );
