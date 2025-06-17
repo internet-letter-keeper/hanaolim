2d72174b2df2f10ac74e75465a101e3dcc8ac2f7
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2025-06-17 14:30:57 +0900

    Chore: Toast 컴포넌트 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
-import { Toast } from "@/components/common";
 import { Card, Olim, Profile, Savings } from "@/components/home";
 
 export default function Page() {
   return (
     <>
       <div className="flex flex-col relative gap-[15px]">
-        <Toast />
         <Image
           src="/images/ic-hanaolim-logo.svg"
           alt="편지 아이콘"
