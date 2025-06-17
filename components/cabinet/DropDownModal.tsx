ec2ccc0f4550f3d268cb2a74e89bb20ae736085c
JungSoEun <sod00402@konkuk.ac.kr>
2025-06-17 10:48:23 +0900

    Feat: hambugi component Publishing (#33)
    * Chore: Shadcn SideBar Install
    
    * Refactor: Add PrimaryButton Color Type
    
    * Chore: Add Icon ic-white-byeoldol.svg
    
    * Chore: Shadcn Global.css
    
    * Feat: Default DropDownModal
    
    * Feat: Default Profile
    
    * Style: Button Style 수정
    
    * Refactor: date 계산 함수 utils로 분리
    
    * Feat: sidebar의 Profile 컴포넌트
    
    * Chore: svg 파일
    
    * Feat: 웹뷰일 경우 SideBar
    
    * Feat: DropDownModal
    
    * Fix: Resize 시 오류 수정
    
    * Style: 글자, 버튼 스타일 수정
    
    * Feat: 햄버거바 모달 추가
    
    * Chore: 경로 수정
    
    * Feat: Router 설정
    
    * Feat: 모달 기본값 열려 있지 않음으로 변경
    
    * Fix: h-full 로 변경
    
    * Fix: 첫장과 두번째 장의 UI가 움직이는 현상
    
    * Feat: 클립보드 임시
    
    * Chore: TODO 적기
    
    * Fix: margin대신 div로 바꾸기
    
    * Feat: NewIcon
    
    * Refactor: Primary Icon 필드 추가
    
    * Refactor: size 조절 추가
    
    * Chore: Todo 추가
    
    * Feat: 햄버거 바 New 아이콘 추가

---
+++ components/cabinet/DropDownModal.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarMenu,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
+import PrimaryButton from "../atoms/PrimaryButton";
+import { NewIcon } from "../common";
+import Profile from "../common/Profile";
+
+const ButtonStyle = "border border-gray-353/25 py-3";
+
+export default function DropDownModal() {
+  // TODO: 세션 프로바이더로 나중에 받아와서 구분하기
+
+  const router = useRouter();
+  const goToLetters = () => router.push("/letters");
+  const goToRegisterSoldier = () => router.push("/registerSoldier");
+  const goToFriends = () => router.push("/friends");
+
+  const { toggleSidebar } = useSidebar();
+  return (
+    <div className="flex justify-center items-center">
+      <Sidebar>
+        <SidebarContent>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <div className="flex flex-col m-7 mt-10 gap-6">
+                <Profile userName="별돌이" endDate={new Date()} />
+                <div className="flex flex-col gap-2">
+                  <PrimaryButton
+                    title="편지 보관함"
+                    className={cn(ButtonStyle, "gap-1")}
+                    textSize={16}
+                    color="gray"
+                    onClick={goToLetters}
+                    // TODO:답장이 온 편지가 있냐 없냐에 따른 Icon 보일지 여부 분기 처리, isNewMessage 같은거 추후 추가
+                    icon={<NewIcon />}
+                  />
+                  <div className="flex gap-2">
+                    {/* TODO: 군인일 경우 버튼 '군인으로 등록하기'안보이게 분기 처리 */}
+                    <PrimaryButton
+                      title="군인으로 등록하기"
+                      className={ButtonStyle}
+                      color="gray"
+                      textSize={16}
+                      onClick={goToRegisterSoldier}
+                    />
+                    <PrimaryButton
+                      title="친구 관리"
+                      className={ButtonStyle}
+                      color="gray"
+                      textSize={16}
+                      onClick={goToFriends}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="flex justify-center">
+                <div
+                  className="bg-gray-353/25 h-1 rounded-[2px] mb-[6px] w-1/5 cursor-pointer"
+                  onClick={toggleSidebar}
+                />
+              </div>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarContent>
+      </Sidebar>
+    </div>
+  );
+}
---
+++ components/common/NewIcon.tsx
@@ -0,0 +1,27 @@
+import { cn } from "@/lib/utils";
+import { Txt } from "../atoms";
+
+type Props = {
+  className?: string;
+  size?: number;
+};
+
+/**
+ * 새로운 편지 아이콘
+ * @param className 아이콘 크기 조절하기
+ * @param size 아이콘 크기
+ */
+export default function NewIcon({ className, size = 10 }: Props) {
+  return (
+    <div
+      className={cn(
+        "rounded-full bg-red-a76 flex items-center justify-center z-10 w-5 h-5",
+        className
+      )}
+    >
+      <Txt className="text-white" size={size} weight="heavy">
+        N
+      </Txt>
+    </div>
+  );
+}
