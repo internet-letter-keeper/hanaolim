992369f644aefc0ca9082dd17004ca2962cfef1f
cheskt <andy9793@naver.com>
2025-06-17 10:59:33 +0900

    Refactor: letterList -> lettersItem 으로 이름 변경경

--- components/letters/index.ts
+++ components/letters/index.ts
@@ -1,3 +1,3 @@
 export { default as FilePreview } from "./FilePreview";
 export { default as IconPicker } from "./IconPicker";
-export { default as LettersList } from "./LettersList";
+export { default as LettersItem } from "./LettersItem";
