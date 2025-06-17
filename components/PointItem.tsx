e50402d64bc7268185d4a7f94e6051bfd559cfdf
Ahhyun Im <66423140+philosophy1106@users.noreply.github.com>
2025-06-17 14:40:57 +0900

    Feat: pointHistory 페이지 퍼블리싱  (#25)
    * Design: global.css 누락된 색상 추가
    
    * Feat: PointItem 컴포넌트 생성
    
    * Refactor: dummy data 분리
    
    * Refactor: 고유한 key 값 설정, item destructuring
    
    * Refactor: PointItem 컴포넌트에 border-b 적용하여 구조 간소화
    
    * Refactor: PointItem type 분리
    
    * Design: PointItem 패딩 값 조절, border 분리
    
    * Publish: 포인트 내역 화면 퍼블리싱
    
    * Design: PointItem 컴포넌트 패딩 값 조정
    
    * Refactor: border 상위 div로 옮김
    
    * Design: 내역 수가 적을 때 하단에 흰색이 아니라 기본 배경색 보이는 문제 해결
    
    * Refactor: 필요 없는 div 삭제
    
    * Desgin: 리스트 하단 여백 추가

--- components/PointItem.tsx
+++ components/PointItem.tsx
@@ -1,12 +1,5 @@
+import type { PointItem } from "@/types/point";
 import Txt from "./atoms/Text";
-
-//TODO: description 통일할지 논의 후 변경
-type PointItem = {
-  description: string;
-  date: Date;
-  amount: number;
-  balance: number;
-};
 
 type Props = {
   item: PointItem;
@@ -24,22 +17,24 @@ function formatDate(date: Date): string {
 }
 
 export default function PointItem({ item }: Props) {
-  const { description, date, amount, balance } = item;
+  const { date, amount, balance } = item;
   return (
-    <div className="flex flex-row justify-between py-[14px] px-7 border-b border-gray-ada ">
-      <Txt size={18} weight="cm">
-        {description}
-      </Txt>
-      <div className="flex flex-col items-end">
-        <Txt size={12} weight="light" className=" text-blue-9a0">
-          {formatDate(date)}
+    <div className="pt-[10px] px-7">
+      <div className="flex flex-row justify-between border-b border-gray-ada">
+        <Txt size={18} weight="cm">
+          하나은행 편지 이벤트
         </Txt>
-        <Txt size={16} weight="bold" className=" mt-[21px] text-green-49d">
-          +{amount.toLocaleString()} 원
-        </Txt>
-        <Txt size={13} weight="cm" className="text-blue-9a0">
-          {balance.toLocaleString()} 원
-        </Txt>
+        <div className="flex flex-col items-end">
+          <Txt size={12} weight="light" className=" text-blue-9a0">
+            {formatDate(date)}
+          </Txt>
+          <Txt size={16} weight="bold" className=" mt-[21px] text-green-49d">
+            +{amount.toLocaleString()} 원
+          </Txt>
+          <Txt size={13} weight="cm" className="mb-3.5 text-blue-9a0">
+            {balance.toLocaleString()} 원
+          </Txt>
+        </div>
       </div>
     </div>
   );
