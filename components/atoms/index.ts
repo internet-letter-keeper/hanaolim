b0da391bcd1d9f89fd4729c87df399f799f68fed
ryd10227 <ryd10227@gmail.com>
2025-06-17 17:45:19 +0900

    HOTFIX: atom/Button.tsx 삭제 index.ts에도 반영

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -1,4 +1,3 @@
-export { default as Button } from "./Button";
 export { default as Input } from "./Input";
 export { default as PrimaryButton } from "./PrimaryButton";
 export { default as Txt } from "./Text";
