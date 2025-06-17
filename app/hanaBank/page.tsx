ab036f6ad35c3b595bcc83aca10ff601acc6d609
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2025-06-17 15:10:54 +0900

    Feat: 하나원큐 페이지

---
+++ app/hanaBank/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function HanaBankPage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <Image
+        src="/icons/hanaBank.svg"
+        alt="하나은행 로고"
+        width={190}
+        height={190}
+      />
+    </div>
+  );
+}
