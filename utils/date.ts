7a97860f1c31729db7787543050baedf73945ec0
ryd10227 <ryd10227@gmail.com>
2025-06-20 09:42:48 +0900

    Feat: 입대일 기준 계급 계산하는 유틸 함수 생성

--- utils/date.ts
+++ utils/date.ts
@@ -1,3 +1,5 @@
+import { SoldierRank } from "@/types/common/profile";
+
 export const untilEndDate = (endDate: Date) =>
   endDate
     ? Math.floor(
@@ -10,3 +12,30 @@ export const dDayConCatString = (endDate: Date) =>
   untilEndDate(endDate) < 0
     ? `D${untilEndDate(endDate)}`
     : `D+${untilEndDate(endDate)}`;
+
+export function calculateRankByStartDate(
+  startDate: Date,
+  currentDate = new Date()
+): SoldierRank {
+  const startDay = startDate.getDate();
+
+  // 첫 진급 기준: 1일이면 2개월 후, 아니면 3개월 후
+  const firstPromotionDate = new Date(startDate);
+
+  firstPromotionDate.setMonth(
+    firstPromotionDate.getMonth() + (startDay === 1 ? 2 : 3)
+  );
+
+  const secondPromotionDate = new Date(firstPromotionDate);
+
+  secondPromotionDate.setMonth(secondPromotionDate.getMonth() + 6);
+
+  const thirdPromotionDate = new Date(secondPromotionDate);
+
+  thirdPromotionDate.setMonth(thirdPromotionDate.getMonth() + 6);
+
+  if (currentDate < firstPromotionDate) return "이병";
+  if (currentDate < secondPromotionDate) return "일병";
+  if (currentDate < thirdPromotionDate) return "상병";
+  return "병장";
+}
