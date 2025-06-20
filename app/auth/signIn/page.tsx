8d684536b0c239b2d65c61f5359efb75d89754c1
cheskt <andy9793@naver.com>
2025-06-20 14:13:45 +0900

    Fix: 로그인 credential 로그인 부분 코드 수정

--- app/auth/signIn/page.tsx
+++ app/auth/signIn/page.tsx
@@ -59,10 +59,9 @@ export default function SignInPage() {
       redirect: false,
     });
 
-    // if (result?.error === "CredentialsSignin") {
-    //   alert("이메일 또는 비밀번호가 잘못되었습니다.");
-    // } else
-    if (result?.ok) {
+    if (result?.error === "CredentialsSignin") {
+      alert("이메일 또는 비밀번호가 잘못되었습니다.");
+    } else if (result?.ok) {
       router.push("/");
     }
   };
