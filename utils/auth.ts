76a50f2f8ae2f7335713368505fd9cddbfdacd8f
JungSoEun <sod00402@konkuk.ac.kr>
2025-06-20 15:07:30 +0900

    Refactor: 세션 확인 메서드 분리 (#64)
    * Feat: 세션 확인 함수
    
    * Refactor: errorMessage 상수 분리
    
    * Feat: icon 이름과 id로 매핑하는 유틸리티 함수
    
    * Fix: session을 return
    
    * Fix: session을 return

---
+++ utils/auth.ts
@@ -0,0 +1,14 @@
+import { ERROR_MESSAGES } from "@/constants/errorMessages";
+import { auth } from "@/lib/auth";
+
+/**
+ * 세션을 체크하는 유틸리티 함수
+ * @return session
+ */
+export const requireAuth = async () => {
+  const session = await auth();
+  if (!session?.user.userId) {
+    throw new Error(ERROR_MESSAGES.AUTH.LOGIN_REQUIRED);
+  }
+  return session;
+};
