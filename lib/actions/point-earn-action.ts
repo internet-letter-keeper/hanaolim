b6170a922d7f9505b4f25fcc41fca2e1f18003ac
ahhyun <ahhyun1106@gmail.com>
2025-06-22 15:27:22 +0900

    Feat: handleEarnPoint 함수 작성

--- lib/actions/point-earn-action.ts
+++ lib/actions/point-earn-action.ts
@@ -1,22 +1,68 @@
 import prisma from "../db";
 import { Letter } from "../generated/prisma";
 
-//0. NOTE:포인트 적립 핸들 함수
-export const handleEarnPoint = async () => {
-  //1. 편지 읽은 날짜 업데이트
-  //2. 적립 가능 여부 확인
-  //3. 적립 액션
+type PointEarnProps = {
+  letterId: number;
+  soldierId: number;
 };
 
-//1.NOTE: 편지 읽었을 때 첫 번째로 수행할 로직, 날짜 업데이트
+/**
+ * 포인트 적립을 다루는 함수
+ * @param letterId
+ * @param soldierId
+ */
+export const handleEarnPoint = async ({
+  letterId,
+  soldierId,
+}: PointEarnProps) => {
+  //letterId 편지 존재 여부 확인
+  const letter = await prisma.letter.findUnique({ where: { letterId } });
+  if (!letter)
+    throw new Error(`letterId: ${letterId} 편지가 존재하지 않습니다`);
+
+  //1. 편지 읽은 날짜 업데이트
+  const readResult = await postReadDate(letterId);
+  if (!readResult.success || !readResult.updated) {
+    return {
+      success: false,
+      message: "이미 읽은 편지거나 업데이트가 실패했습니다",
+    };
+  }
+
+  //2. 적립 가능 여부 확인
+  const earnableResult = await getPointEarnability(letter);
+  if (!earnableResult.success || !earnableResult.earnability)
+    return {
+      success: false,
+      reason: "포인트 적립 조건 불충족",
+    };
+  //3. 적립 액션
+  const earnResult = await postEarnedPoint(soldierId);
+  if (!earnResult.succes) {
+    return {
+      success: false,
+      reason: "포인트 적립 실패",
+    };
+  }
+  //최종 적립 성공 후
+  return {
+    success: true,
+    bonus: earnResult.bonus,
+  };
+};
 
 /**
  * readDate가 null인 Letter를 현재 시간으로 업데이트
  * @param letterId 편지 ID
  * @returns 업데이트 성공 시 succss, 업데이트 실행시 success
- * @throws 실패 시 false
+ * @throws 유효하지 않은 letterId 형식 또는 DB 처리 중 에러 발생
  */
-export const postReadDate = async (letterId: number) => {
+const postReadDate = async (letterId: number) => {
+  //letterId 형식 확인하기
+  if (typeof letterId !== "number" || Number.isNaN(letterId)) {
+    throw new Error("letterId는 숫자여야 합니다");
+  }
+
   try {
     const res = await prisma.letter.updateMany({
       where: {
@@ -38,8 +84,6 @@ export const postReadDate = async (letterId: number) => {
   }
 };
 
-//2. NOTE: 1번에서 날짜가 업데이트 됐으면 적립 가능 여부 확인하기
-
 /**
  * 안 읽은 편지라면 포인트 적립 조건 검사하기
  * @param letterId 편지 ID
@@ -48,7 +92,7 @@ export const postReadDate = async (letterId: number) => {
  * @returns 함수 실행 성공 여부와 포인트 적립 가능 여부
  * @throws 함수 실행 실패 시 false
  */
-export const getPointEarnability = async ({
+const getPointEarnability = async ({
   letterId,
   senderId,
   receiverId,
@@ -92,14 +136,13 @@ export const getPointEarnability = async ({
   }
 };
 
-//3. NOTE: 2번까지 수행됐으면 포인트 적립 실행
 /**
  * 포인트 적립 조건을 만족했을 때 경험치를 업데이트하고 포인트를 적립해 줌
  * @param soldierId
  * @returns 경험치 적립 성공 여부와 적립한 포인트
  * @throws 경험치 적립 실패 또는 포인트 적립 실패
  */
-export const postEarnedPoint = async (soldierId: number) => {
+const postEarnedPoint = async (soldierId: number) => {
   try {
     //3-1. 경험치 적립 실행
     const res = await prisma.soldier.update({
