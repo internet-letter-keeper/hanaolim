a63a29fde7e03f41ec8f8c6d649e3df702698a30
soobin <subin3620@naver.com>
2025-06-23 10:40:46 +0900

    Feat: 군적금 서버액션 추가 및 나사카 군적금 서버액션 병합

--- lib/actions/home-actions.ts
+++ lib/actions/home-actions.ts
@@ -13,6 +13,8 @@ type CardInfo = {
   accountBalance: number;
 };
 
+type Savings = { savingsBalance: number };
+
 /**
  * 홈화면에 표시할 유저 이름 + 병사 날짜 정보 조회
  * @usage 홈 상단 프로필
@@ -51,31 +53,34 @@ export const getProfileInfo = async (userId: number): Promise<ProfileInfo> => {
 };
 
 /**
- * 하나 나라사랑카드 정보 조회
- * @usage 카드 컴포넌트
+ * 유저의 계좌 및 적금 정보 조회
+ * @usage 카드 컴포넌트, 적금 컴포넌트
  * @param userId number - 유저 ID
- * @returns { accountNum, accountBalance }
- * @throws 군 복무 정보 또는 계좌 정보가 없을 경우
+ * @returns { accountNum, accountBalance, savingsBalance }
+ * @throws userId에 해당하는 군 복무 정보 또는 계좌 정보가 없을 경우
  */
-export const getCardInfo = async (userId: number): Promise<CardInfo> => {
+export const getAccountInfo = async (
+  userId: number
+): Promise<{
+  accountNum: string;
+  accountBalance: number;
+  savingsBalance: number;
+}> => {
   const soldier = await prisma.soldier.findFirst({
     where: { userId },
     select: {
-      soldierId: true,
       Account: {
         select: {
           accountNum: true,
           accountBalance: true,
+          savingsBalance: true,
         },
       },
     },
   });
 
-  if (!soldier) {
-    throw new Error(`${userId}번 유저의 군 복무 정보가 없습니다`);
-  }
+  const account = soldier?.Account?.[0];
 
-  const account = soldier.Account?.[0];
   if (!account) {
     throw new Error(`${userId}번 유저의 계좌 정보가 없습니다`);
   }
@@ -83,5 +88,6 @@ export const getCardInfo = async (userId: number): Promise<CardInfo> => {
   return {
     accountNum: account.accountNum,
     accountBalance: account.accountBalance,
+    savingsBalance: account.savingsBalance,
   };
 };
