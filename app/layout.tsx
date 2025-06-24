8ac75f9ab51a54cda5cd1c6c11e6204545c2848f
JungSoEun <sod00402@konkuk.ac.kr>
2025-06-25 08:56:38 +0900

    Feat: onboarding add friend (#105)
    * Fix: 전역일, 입대일 수정 시 반영 에러
    
    * Refactor: 해당하는 군인이 있는지 확인
    
    * Feat: 로그아웃 연결
    
    * Refactor: follow 반환
    
    * Refactor: ToastProvider layout 으로 이동
    
    * Style: Toast 스타일 수정
    
    * Feat: onboarding 군인 등록
    
    TODO: 친구 추가와 공통로직 빼야함

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
+import { ToastProvider } from "@/contexts/toast/ToastProvider";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -15,9 +16,11 @@ export default function RootLayout({
     <html lang="en">
       <body className="flex items-center justify-center h-screen overflow-hidden antialiased">
         <SessionProvider>
+          <ToastProvider>
           <div className="h-full w-full sm:max-w-sm p-4 bg-white-afa shadow-[0_0_10px_rgba(0,0,0,0.3)] overflow-y-auto [&::-webkit-scrollbar]:hidden">
             {children}
           </div>
+          </ToastProvider>
         </SessionProvider>
       </body>
     </html>
