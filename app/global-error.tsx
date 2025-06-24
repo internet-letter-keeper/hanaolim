5b5fe7649bde11852980cc00441611c8cdb82d88
soobin <subin3620@naver.com>
2025-06-24 17:47:07 +0900

    Feat: global error 페이지 구현

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,14 +1,39 @@
 "use client";
 
-export default function GlobalError({ error }: { error: Error }) {
+import Image from "next/image";
+import { Txt } from "@/components/atoms";
+
+export default function NotFound() {
   return (
-    <html>
-      <body>
-        <div style={{ padding: "2rem", textAlign: "center" }}>
-          <h1>⚠️ 에러가 발생했습니다</h1>
-          <p>{error.message}</p>
-        </div>
-      </body>
-    </html>
+    <div className="h-screen flex flex-col justify-center items-center text-center gap-4 px-6">
+      <br />
+      <br />
+
+      <Image
+        src="/images/byeoldol-error.svg"
+        alt="매우 놀란 비상 별돌이 이미지"
+        width={100}
+        height={100}
+      />
+      <Txt size={60} weight="heavy" className="text-red-a76">
+        ERROR
+      </Txt>
+      <Txt size={22} weight="bold">
+        <br />
+        에러가 발생했습니다.
+      </Txt>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Txt size={15} weight="medium" className="text-red-a76 mt-15">
+        요청하신 페이지를 처리 중에 오류가 발생했습니다.
+        <br />
+        서비스 이용에 불편을 드려 죄송합니다.
+        <br />
+        <br />
+        다시 시도해 주시기 바랍니다.
+      </Txt>
+    </div>
   );
 }
