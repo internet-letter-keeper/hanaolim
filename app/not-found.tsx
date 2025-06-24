fb16512c772da4ebc0710e3ec00bf95178302016
soobin <subin3620@naver.com>
2025-06-25 08:59:38 +0900

    Design: 에러페이지 디자인 수정

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -13,20 +13,20 @@ export default function NotFound() {
 
   return (
     <div className="h-screen flex flex-col justify-center items-center text-center gap-4 px-6">
-      <Txt size={20} weight="bold">
-        앗! 길을 잃었어요..
-      </Txt>
-      <Txt size={100} weight="heavy" className="text-green-49d">
-        404
-      </Txt>
       <Image
         src="/images/byeoldol-sosad.svg"
         alt="매우 슬픈 별돌이 이미지"
         width={100}
         height={100}
       />
+      <Txt size={60} weight="heavy" className="text-green-49d">
+        404
+      </Txt>
+      <Txt size={20} weight="bold">
+        앗! 길을 잃었어요..
+      </Txt>
 
-      <Txt size={18} weight="medium" className="text-green-49d">
+      <Txt size={18} weight="medium" className="text-green-49d mt-15">
         요기있는 별돌이가 다시 데려다줄게요!
       </Txt>
 
@@ -37,7 +37,7 @@ export default function NotFound() {
         textSize={16}
         align="center"
         weight="cm"
-        className="h-[38px] w-2/3 mt-20"
+        className="h-[38px] w-2/3 mt-10"
       />
     </div>
   );
