16cb94a5b64be46cc9be64d1a399c97a8cfe8289
soobin <subin3620@naver.com>
2025-06-24 17:39:06 +0900

    Feat: notfound 페이지 구현

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,44 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { PrimaryButton, Txt } from "@/components/atoms";
+
 export default function NotFound() {
+  const router = useRouter();
+
+  const handleGoToHome = () => {
+    router.push("/");
+  };
+
   return (
-    <div className="p-8 text-center">
-      <h1 className="text-2xl font-bold">페이지를 찾을 수 없습니다 🧐</h1>
-      <p className="mt-2 text-gray-600">주소를 다시 확인해주세요.</p>
+    <div className="h-screen flex flex-col justify-center items-center text-center gap-4 px-6">
+      <Txt size={20} weight="bold">
+        앗! 길을 잃었어요..
+      </Txt>
+      <Txt size={100} weight="heavy" className="text-green-49d">
+        404
+      </Txt>
+      <Image
+        src="/images/byeoldol-sosad.svg"
+        alt="매우 슬픈 별돌이 이미지"
+        width={100}
+        height={100}
+      />
+
+      <Txt size={18} weight="medium" className="text-green-49d">
+        요기있는 별돌이가 다시 데려다줄게요!
+      </Txt>
+
+      <PrimaryButton
+        title="홈으로 이동하기"
+        onClick={handleGoToHome}
+        rounded="sm"
+        textSize={16}
+        align="center"
+        weight="cm"
+        className="h-[38px] w-2/3 mt-20"
+      />
     </div>
   );
 }
