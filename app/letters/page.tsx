e19e93b26245e05e9778dbd04a29d514204b2ca2
cheskt <andy9793@naver.com>
2025-06-25 15:36:56 +0900

    Refactor: 페이지 컴포넌트 수정 및 삭제 작업

--- app/letters/page.tsx
+++ app/letters/page.tsx
@@ -1,11 +1,12 @@
+import { Txt } from "@/components/atoms";
 import { BasicHeader } from "@/components/common";
 import {
-  AllLettersCnt,
   FilterBtn,
   LetterboxTabSelector,
-  LettersList,
+  LettersItem,
   SearchLetter,
 } from "@/components/letters";
+import { getFilteredLetters } from "@/lib/actions/letter-actions";
 import { requireAuth } from "@/utils/auth";
 
 type Props = {
@@ -15,17 +16,51 @@ type Props = {
 export default async function LettersPage({ searchParams }: Props) {
   const box = (searchParams.box as "mine" | "friend") ?? "mine";
   const query = (searchParams.query as string) ?? "";
+  const filter = searchParams.filter;
+
   const session = await requireAuth();
   const currentUserId = session?.user?.userId!;
+
+  const filteredLetters = await getFilteredLetters({
+    box,
+    userId: currentUserId,
+    isFavorite: filter === "favorite",
+    isUnread: filter === "unread",
+    hasReply: filter === "hasReply",
+    query,
+  });
+
+  const { data, ok } = filteredLetters;
+
+  if (!ok) {
+    return <Txt className="text-gray-400">편지를 불러오지 못했어요</Txt>;
+  }
 
   return (
     <div className="flex flex-col gap-4">
       <BasicHeader title="편지 보관함" />
       <LetterboxTabSelector box={box} />
       <SearchLetter />
-      {/* <AllLettersCnt total={data.length} /> */}
-      <FilterBtn />
-      <LettersList searchParams={searchParams} currentUserId={currentUserId} />
+      <div className="flex flex-col gap-2">
+        <Txt weight="cm" align="left" size={13} className="px-1">
+          총 {data?.length}개
+        </Txt>
+        <FilterBtn />
+      </div>
+      <div className="flex flex-col gap-3 pb-8">
+        {data?.length === 0 ? (
+          <Txt className="text-gray-400">조건에 맞는 편지가 없습니다</Txt>
+        ) : (
+          data?.map((letter: any) => (
+            <LettersItem
+              key={letter.letterId}
+              box={box}
+              letter={letter}
+              currentUserId={currentUserId}
+            />
+          ))
+        )}
+      </div>{" "}
     </div>
   );
 }
