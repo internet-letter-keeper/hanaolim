c1d04d5674520348217a6a228f4e7a6dff258acd
ahhyun <ahhyun1106@gmail.com>
2025-06-25 15:16:10 +0900

    Design: 포인트 내역 없을 때, 친구 목록 없을 때 아이콘 좀 더 아래로 (세로축 기준 중앙)

--- app/pointHistory/page.tsx
+++ app/pointHistory/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import EmptyState from "@/components/EmptyList";
 import PointItem from "@/components/PointItem";
 import { Txt } from "@/components/atoms";
@@ -24,19 +25,27 @@ export default async function PointHistoryPage() {
         {pointSum.toLocaleString()} 원
       </Txt>
       <div className="flex-1 flex flex-col bg-white-fff -m-4 pb-8">
-        <div>
-          {/* 잔액/내역 구분선 */}
-          <div className="h-[1px] bg-gray-530 mb-4 mt-7 mx-7" />
-        </div>
-        {/* 포인트 입금 내역 */}
+        {/* 구분선 - 포인트 입금 내역 */}
         {pointList.length > 0 ? (
-          pointList.map((item) => (
-            <PointItem key={`${item.pointId}`} item={item} />
-          ))
+          <>
+            <div className="h-[1px] bg-gray-530 mb-4 mt-7 mx-7" />
+            {pointList.map((item) => (
+              <PointItem key={`${item.pointId}`} item={item} />
+            ))}
+          </>
         ) : (
-          <EmptyState>
-            포인트 내역이 없어요 <br /> 편지를 읽고 포인트를 받아 보세요!
-          </EmptyState>
+          <div className="flex flex-col justify-center items-center gap-6">
+            <EmptyState>
+              포인트 내역이 없어요 <br /> 편지를 읽고 포인트를 받아 보세요!{" "}
+              <br />
+            </EmptyState>
+            <Link
+              href={`/cabinet/${soldierId}`}
+              className="bg-green-49d text-white-fff p-2 rounded-[8px] text-[14px]"
+            >
+              편지 읽으러 가기
+            </Link>
+          </div>
         )}
       </div>
       {/* TODO: 무한스크롤 - 데이터 가져오는 중 */}
