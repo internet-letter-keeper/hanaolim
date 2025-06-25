35b69503505eaa8c8ed860c0230e47d178406dfa
cheskt <andy9793@naver.com>
2025-06-25 14:00:47 +0900

    Refactor: 편지 상세 조회 페이지 이미지 추가

--- app/letters/[letterId]/page.tsx
+++ app/letters/[letterId]/page.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
 import { PrimaryButton } from "@/components/atoms";
 import BasicHeader from "@/components/common/BasicHeader";
 import LettersDetail from "@/components/letters/LettersDetail";
 import { getLetterDetail } from "@/lib/actions/letter-actions";
+import { handleEarnPoint } from "@/lib/actions/point-earn-action";
 import { requireAuth } from "@/utils/auth";
 
 type Props = {
@@ -20,6 +22,9 @@ export default async function LetterDetailPage({ params }: Props) {
   if (!letter || !letter.data)
     throw new Error("편지 정보를 가져오는 것에 실패했습니다");
 
+  if (!letter.data.readDate && soldierId)
+    await handleEarnPoint({ letterId, soldierId });
+
   const reply = await getLetterDetail({ letterId, userId, isReply: true });
 
   return (
@@ -29,6 +34,20 @@ export default async function LetterDetailPage({ params }: Props) {
       {/* 원본 편지 */}
       <div className="py-4 flex justify-center">
         <div className="bg-white shadow-sm p-4 w-[90%] max-w-md">
+          <div className="flex justify-center gap-4 mb-3">
+            <Image
+              src="/images/byeoldol-face.svg"
+              alt="별돌이 얼굴"
+              width={50}
+              height={50}
+            />
+            <Image
+              src="/images/letter.svg"
+              alt="별돌이 얼굴"
+              width={50}
+              height={50}
+            />
+          </div>
           <LettersDetail letter={letter.data} />
         </div>
       </div>
