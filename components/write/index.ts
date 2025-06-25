cdeb615a936e65a0632803bdd38729321e4afac0
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2025-06-26 01:48:44 +0900

    Chore: index.ts 지정

---
+++ components/write/index.ts
@@ -0,0 +1,4 @@
+export { default as FileUploadSection } from "./FileUploadSection";
+export { default as IconSelectionSection } from "./IconSelectionSection";
+export { default as LetterForm } from "./LetterForm";
+export { default as LetterPageLayout } from "./LetterPageLayout";
