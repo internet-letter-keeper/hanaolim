677042191ec61152edc41a951b8211261f134571
Ausdauer1 <steady051@naver.com>
2025-06-25 17:13:56 +0900

    Refactor: 사용중 이메일 메세지 알림 위치 수정, 로그인/회원가입 callbackUrl 적용

--- app/auth/signUp/page.tsx
+++ app/auth/signUp/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useRef, useState, KeyboardEvent } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { signIn } from "next-auth/react";
+import { useState, KeyboardEvent } from "react";
 import SignUpInput from "@/components/SignUpInput";
-import { Input, PrimaryButton, Txt } from "@/components/atoms";
+import { PrimaryButton, Txt } from "@/components/atoms";
 import { isEmailDuplicated, postSignUp } from "@/lib/actions/auth-actions";
+import { postFriendbyId } from "@/lib/actions/friend-actions";
 import {
   checkEmailValidation,
   checkNameValidation,
@@ -14,22 +16,29 @@ import {
 
 export default function SignUpPage() {
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") || "/";
 
   const goToSignIn = () => {
-    router.push("/auth/signIn");
+    if (callbackUrl && callbackUrl !== "/") {
+      router.push(
+        `/auth/signIn?callbackUrl=${encodeURIComponent(callbackUrl)}`
+      );
+    } else {
+      router.push("/auth/signIn");
+    }
   };
 
   const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const passwordRef = useRef<HTMLInputElement>(null);
-  const confirmPasswordRef = useRef<HTMLInputElement>(null);
-
   const [nameError, setNameError] = useState(false);
-  const [emailError, setEmailError] = useState(false);
   const [nameMessage, setNameMessage] = useState("");
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState(false);
+  const [emailMessage, setEmailMessage] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [signUpError, setSignUpError] = useState("");
+
+  const [isLoading, setIsLoading] = useState(false);
 
   const passwordValidation = checkPasswordValidation(password);
   const isPasswordMatch = password === confirmPassword;
@@ -51,35 +60,17 @@ export default function SignUpPage() {
     checkNameValidation(name).valid &&
     checkEmailValidation(email) &&
     passwordValidation.valid &&
-    isPasswordMatch;
+    isPasswordMatch &&
+    !isLoading;
 
   // 회원가입 버튼 클릭 핸들러
   const handleSignUp = async () => {
-    // 이름 유효성 검사
-    const nameValidation = checkNameValidation(name);
-    if (nameValidation.valid) {
-      setNameError(false);
-      setNameMessage("");
-    } else {
-      setNameError(false);
-      setNameError(true);
-      setNameMessage(nameValidation["message"]);
-      return;
-    }
-
-    // 이메일 유효성 검사
-    if (checkEmailValidation(email)) {
-      setEmailError(false);
-    } else {
-      setEmailError(true);
-      return;
-    }
-
-    setSignUpError("");
-    // 회원가입 api 호출
+    setIsLoading(true);
     const duplicated = await isEmailDuplicated(email);
     if (duplicated) {
-      setSignUpError("이미 사용 중인 이메일입니다.");
+      setEmailError(true);
+      setEmailMessage("이미 사용 중인 이메일입니다.");
+      setIsLoading(false);
       return;
     }
     const result = await postSignUp({
@@ -87,10 +78,34 @@ export default function SignUpPage() {
       userName: name,
       password: password || "",
     });
+    if (callbackUrl && callbackUrl !== "/") {
+      await postFriendbyId(
+        Number(callbackUrl.split("/").pop()),
+        Number(result.data?.userId)
+      );
+    }
     if (result.ok) {
-      router.push("/auth/signIn");
+      const signInResult = await signIn("credentials", {
+        email,
+        password,
+        callbackUrl,
+        redirect: false,
+      });
+      if (signInResult?.ok) {
+        // 로그인 성공 후 리다이렉트
+        if (signInResult.url) {
+          setIsLoading(false);
+          router.push(signInResult.url);
+        } else {
+          setIsLoading(false);
+          router.push("/onboarding");
+        }
+      } else {
+        router.push("/auth/error");
+      }
     } else {
-      setSignUpError(result.error || "회원가입에 실패했습니다.");
+      setIsLoading(false);
+      router.push("/auth/error?type=signup");
       return;
     }
   };
@@ -129,9 +144,7 @@ export default function SignUpPage() {
               setNameError(true);
               setNameMessage(validation.message);
             }
-            setSignUpError("");
           }}
-          onFocus={() => setSignUpError("")}
           placeholder="이름을 입력해주세요"
           maxLength={8}
           error={nameError}
@@ -144,13 +157,12 @@ export default function SignUpPage() {
           onChange={(e) => {
             setEmail(e.target.value);
             setEmailError(!checkEmailValidation(e.target.value));
-            setSignUpError("");
+            setEmailMessage("이메일 형식이 올바르지 않습니다.");
           }}
-          onFocus={() => setSignUpError("")}
           placeholder="이메일을 입력해주세요"
           maxLength={30}
           error={emailError}
-          errorMessage="유효하지 않은 이메일 형식입니다."
+          errorMessage={emailMessage}
         />
         {/* 비밀번호 */}
         <SignUpInput
@@ -162,7 +174,6 @@ export default function SignUpPage() {
           error={!!passwordErrorMessage}
           errorMessage={passwordErrorMessage}
           type="password"
-          
         />
         <SignUpInput
           label="비밀번호 확인"
@@ -178,11 +189,6 @@ export default function SignUpPage() {
       </div>
 
       {/* 회원가입 버튼 */}
-      {signUpError && (
-        <Txt size={12} align="left" className="text-red-a76 w-full mt-2">
-          {signUpError}
-        </Txt>
-      )}
       <PrimaryButton
         title="회원가입"
         rounded="sm"
