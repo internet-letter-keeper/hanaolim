c6422bde46f834efeff7444d9b5cfd085e892839
ryd10227 <ryd10227@gmail.com>
2025-06-28 19:00:01 +0900

    Refactor: 관물대 편지 아이콘 absolute 좌표 위치 객체를 배열로 변경, 함수 제거

--- types/common/icons.ts
+++ types/common/icons.ts
@@ -12,16 +12,10 @@ export type IconName =
  * name: icon 의 이름
  * src: icon의 경로
  * alt: icon 대체 텍스트
- * className: 관물대에서의 newIcon의 위치
  */
 export type Icon = {
   id: number;
   name: IconName;
   src: string;
   alt: string;
-};
-
-export type IconPosition = {
-  positionId: number;
-  iconPosition: string;
 };
