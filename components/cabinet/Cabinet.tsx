98193e41e2fd6e46589e0b0fe31f19a45efb31a9
ryd10227 <ryd10227@gmail.com>
2025-06-28 18:55:51 +0900

    Refactor: 관물대 페이지네이션을 서버컴포넌트로 변경해 편지 렌더링 속도 높임

--- components/cabinet/Cabinet.tsx
+++ components/cabinet/Cabinet.tsx
@@ -5,46 +5,34 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { Txt } from "@/components/atoms";
+import { ICON_POSITION } from "@/constants/icons";
 import { useToast } from "@/contexts/toast/ToastContext";
 import { postFriendbyId } from "@/lib/actions/friend-actions";
-import {
-  getNonReplyLettersByUserId,
-  getTotalReceivedNonReplyLettersCnt,
-} from "@/lib/actions/letter-actions";
-import { getIconInfoByIconId, getIconPositionByIdx } from "@/utils/icon";
+import { getLettersByUserId } from "@/lib/actions/letter-actions";
+import { getIconInfoByIconId } from "@/utils/icon";
 import { NewIcon } from "../common";
 import LetterModal from "../letters/LetterModal";
 
 type Props = {
-  isMyCabinet: boolean;
   userId: number;
   soldierId: number;
+  letters: Awaited<ReturnType<typeof getLettersByUserId>>["data"];
 };
 
-export default function Cabinet({ isMyCabinet, userId, soldierId }: Props) {
+export default function Cabinet({ userId, soldierId, letters }: Props) {
   const { data, update } = useSession();
 
   const loginUserId = data?.user.userId;
 
-  const [currentPage, setCurrentPage] = useState(1);
+  const loginSoldier = data?.user.soldier;
 
-  const [totalPage, setTotalPage] = useState(1);
-
-  const [totalLettersCnt, setTotalLettersCnt] = useState(0);
+  const isMyCabinet = loginSoldier
+    ? loginSoldier.soldierId === soldierId
+    : false;
 
   const { showToast } = useToast();
-  const router = useRouter();
 
-  const toPrevCabinet = () => setCurrentPage((prev) => prev - 1);
-
-  const toNextCabinet = () => setCurrentPage((prev) => prev + 1);
-
-  const [currentPageLetters, setCurrentPageLetters] =
-    useState<Awaited<ReturnType<typeof getNonReplyLettersByUserId>>["data"]>();
-
-  const [isModalOpened, setModalOpened] = useState(false);
-
-  const [isOpenedLetterId, setOpenedLetterId] = useState(0);
+  const [openedLetterId, setOpenedLetterId] = useState<number | null>(null);
 
   const [optimisticallyReadIds, setOptimisticallyReadIds] = useState<number[]>(
     []
@@ -52,12 +40,19 @@ export default function Cabinet({ isMyCabinet, userId, soldierId }: Props) {
 
   const onLetterClick = (letterId: number) => {
     if (!isMyCabinet) {
-      showToast("내가 작성한 편지가 아니에요", "", "error");
+      showToast("편지 수신인만 열어볼 수 있어요", "", "warning");
+      return;
+    }
+
+    const isAfterStartDate =
+      new Date(data?.user.soldier.startDate!) > new Date();
+
+    if (isMyCabinet && isAfterStartDate) {
+      showToast("입대일 이후에 열어볼 수 있어요", "", "warning");
       return;
     }
 
     setOpenedLetterId(letterId);
-    setModalOpened(true);
 
     // 읽자마자 'New' 아이콘 숨김 처리
     setOptimisticallyReadIds((prev) => [...prev, letterId]);
@@ -65,71 +60,28 @@ export default function Cabinet({ isMyCabinet, userId, soldierId }: Props) {
 
   const searchParams = useSearchParams();
   const addFollow = searchParams.get("add");
+  const currentPage = searchParams.get("page") ?? "1";
+
+  const router = useRouter();
 
   useEffect(() => {
     (async () => {
-      const totalLettersCntInit =
-        await getTotalReceivedNonReplyLettersCnt(userId);
-
-      setTotalLettersCnt(totalLettersCntInit);
-
-      setTotalPage(
-        totalLettersCntInit <= 7 ? 1 : Math.ceil(totalLettersCntInit / 7)
-      );
-
-      const letters = await getNonReplyLettersByUserId(
-        userId,
-        1,
-        totalLettersCntInit
-      );
-
-      setCurrentPageLetters(letters.data);
-
       if (addFollow === "true" && loginUserId && !data.user.follow) {
         const { follow } = await postFriendbyId(soldierId, loginUserId);
         await update({ ...data?.user, follow });
-      }
 
-      router.refresh();
-    })();
-  }, [addFollow, loginUserId, userId]);
-
-  useEffect(() => {
-    (async () => {
-      const letters = await getNonReplyLettersByUserId(
-        userId,
-        currentPage,
-        totalLettersCnt
-      );
-
-      if (letters.data) {
-        setCurrentPageLetters(letters.data);
+        if (data.user.isSocial) router.refresh();
       }
     })();
-  }, [currentPage, totalLettersCnt, userId]);
+  }, [addFollow, loginUserId, userId, currentPage]);
 
   return (
-    <div className="flex flex-col items-center relative px-10">
-      {isModalOpened && (
+    <div className="relative size-full">
+      {openedLetterId && (
         <LetterModal
-          letterId={isOpenedLetterId}
-          onHandleModal={() => setModalOpened(false)}
+          letterId={openedLetterId}
+          onHandleModal={() => setOpenedLetterId(null)}
         />
-      )}
-
-      {/* 관물대 첫 페이지면 이전 페이지 버튼이 안 보이게 */}
-      {currentPage > 1 ? (
-        <button onClick={toPrevCabinet}>
-          <Image
-            src="/icons/ic-next-page.svg"
-            alt="관물대 이전 페이지로"
-            width={20}
-            height={20}
-            className="absolute w-[5%] scale-x-[-1] left-0 top-[45%]"
-          />
-        </button>
-      ) : (
-        <div />
       )}
 
       <Image
@@ -140,56 +92,32 @@ export default function Cabinet({ isMyCabinet, userId, soldierId }: Props) {
         priority
       />
 
-      {currentPageLetters?.map(
-        ({ letterId, iconId, readDate, nickname }, idx) => {
-          if (!iconId) return null;
+      {letters?.map(({ letterId, iconId, readDate, nickname }, idx) => {
+        if (!iconId) return null;
 
-          const iconPosition = getIconPositionByIdx(idx);
+        const { src, alt } = getIconInfoByIconId(iconId);
 
-          const { src, alt } = getIconInfoByIconId(iconId);
+        const isRead = !!readDate || optimisticallyReadIds.includes(letterId);
 
-          const isRead = !!readDate || optimisticallyReadIds.includes(letterId);
-
-          return (
-            <button
-              key={letterId}
-              onClick={() => onLetterClick(letterId)}
-              className={iconPosition}
+        return (
+          <button
+            key={letterId}
+            onClick={() => onLetterClick(letterId)}
+            className={"absolute w-[22%] h-[14%] " + ICON_POSITION[idx]}
+          >
+            {!isRead && (
+              <NewIcon className="absolute top-0 right-0.5 size-[30%]" />
+            )}
+            <Image src={src} alt={alt} fill />
+            <Txt
+              weight="medium"
+              className="absolute bottom-0 left-2 text-white truncate max-w-[7ch] z-10"
             >
-              {!isRead && (
-                <NewIcon className="absolute top-0 right-0.5 w-[25%] h-[30%]" />
-              )}
-              <Image src={src} alt={alt} fill />
-              <Txt
-                weight="medium"
-                className="absolute bottom-0 left-2 text-white truncate max-w-[7ch] z-10"
-              >
-                {nickname}
-              </Txt>
-            </button>
-          );
-        }
-      )}
-
-      {/* 관물대 마지막 페이지면 다음 페이지 버튼이 안 보이게 */}
-      {currentPage < totalPage && (
-        <button onClick={toNextCabinet}>
-          <Image
-            src="/icons/ic-next-page.svg"
-            alt="관물대 다음 페이지로"
-            width={20}
-            height={20}
-            className="absolute w-[5%] right-0 top-[45%]"
-          />
-        </button>
-      )}
-
-      {/* 페이지네이션 */}
-      <div className="flex items-center mt-2 bg-white-2f2 rounded-[5px] px-3 py-0.5">
-        <Txt className="text-blue-9a0" weight="medium" size={12}>
-          {currentPage}/{totalPage}
-        </Txt>
-      </div>
+              {nickname}
+            </Txt>
+          </button>
+        );
+      })}
     </div>
   );
 }
