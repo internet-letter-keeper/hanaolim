4e7c4d075792de47052113ed373d1375c986c500
ryd10227 <76207466+ryd10227@users.noreply.github.com>
2025-06-29 07:29:39 +0900

    Refactor: cabinet 리팩토링 (#153)
    * Refactor: db서버 주소 변경으로 인한 스크립트 변경
    
    * Rename: cabinet 동적 param을 code->soldierId로 변경
    
    * Feat: 입대일 기준 계급 계산하는 유틸 함수 생성
    
    * fix: 용돈주기, 편지쓰기 아이콘 안 나오는 버그 해결
    
    * Feat: 타입 등록. 계급, 군인 프로필, 친구 프로필
    
    * Feat: 친구 목록 불러오는 서버액션
    
    * Fefactor: 유틸함수 주석 추가
    
    * Remove: gitkeep
    
    * Refactor: 친구 목록 불러오는 서버액션에 에러처리와 주석 추가
    
    * Feat: 임시 글로벌 에러 페이지, 404 not found 페이지 추가
    
    * Rename: 친구 목록 불러오기 액션 파일명 변경
    
    * Refactor: 친구 프로필 원형 아이콘. 필드명 변경으로 인한 수정
    
    * Feat: db에 있는 유저인지 확인하는 서버액션 추가
    
    * Feat: 친구 목록 불러오기 주석 추가
    
    * Feat: 관물대 - 친구 목록 맨 앞에 내 프로필 불러오는 로직 추가
    
    * Refactor: 친구 프로필 원형 아이콘 서버 컴포넌트로 변경
    
    * Feat: 용돈 주기, 편지 쓰기 버튼 기능 구현 완료
    
    * Feat: 캐비넷 헤더 구현 완료
    
    * Feat: 캐비넷 상태 메시지 변경 구현 완료
    
    * Refactor: 관물대에서 편지 쓰기 버튼 클릭 시 이동하는 route 수정
    
    * fix: Soldier 테이블에 code 필드 추가
    
    * Feat: 캐비넷 친구 추가 구현 완료
    
    * Feat: 친구 프로필 원형 아이콘이 선택되면 붉은색으로 변하도록 스타일 수정
    
    * Design: 관물대 헤더 스타일 변경 (사용자 이름)
    
    * Chore: 폴더 추가로 인한 prettierrc import문 정렬 규칙 추가, 캐비넷 컴포넌트에 적용
    
    * Delete: 더미 데이터 날리기
    
    * Feat: 캐비넷에 편지 띄워주기
    
    * Refactor: 편지 모달에서 userId prop 삭제, 세션에서 받아오는 로직으로 변경
    
    * Feat: 미로그인 사용자가 관물대 볼 수 있도록 처리
    
    * Refactor: 주석, 안 쓰는 변수 삭제
    
    * Fix: 관물대 이미지 작아졌다가 커지는 현상 고침
    
    * Fix: 미로그인 - '로그인' 버튼 클릭 시 리다이렉트 링크로 보내도록 수정
    
    * Refactor: 친구 추가 모달에 내 코드를 입력했을 때 처리
    
    * Refactor: 관물대 페이지네이션을 서버컴포넌트로 변경해 편지 렌더링 속도 높임
    
    * Refactor: 관물대 편지 아이콘 absolute 좌표 위치 객체를 배열로 변경, 함수 제거
    
    ---------
    
    Co-authored-by: JungSoEun <sod00402@konkuk.ac.kr>

--- app/cabinet/[soldierId]/page.tsx
+++ app/cabinet/[soldierId]/page.tsx
@@ -1,10 +1,10 @@
 import {
-  Cabinet,
   CabinetHeader,
   FriendsList,
   LetterMoneyButton,
   StatusMessage,
   DropDownModal,
+  CabinetPagenation,
 } from "@/components/cabinet";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { getUserBySoldierId } from "@/lib/actions/friend-actions";
@@ -13,28 +13,30 @@ import { auth } from "@/lib/auth";
 
 type Props = {
   params: Promise<{ soldierId: number }>;
+  searchParams: Promise<{ page: number }>;
 };
 
-export default async function CabinetPage({ params }: Props) {
+export default async function CabinetPage({ params, searchParams }: Props) {
   const { soldierId } = await params;
+  const currentPage = (await searchParams).page ?? "1";
 
-  const { success, message, data } = await getUserBySoldierId(+soldierId);
+  const {
+    success,
+    message,
+    data: soldierInfo,
+  } = await getUserBySoldierId(+soldierId);
 
-  if (!success) {
-    throw new Error(message);
-  }
-
-  const soldierInfo = data!;
+  if (!success || !soldierInfo) throw new Error(message);
 
   const session = await auth();
 
-  const isLoggedIn = session?.user;
-
-  const { isNew } = isLoggedIn ? await getIsNew(+session.user.userId) : {};
+  const isLoggedIn = !!session?.user;
 
   const isMyCabinet = session?.user.soldier
     ? session.user.soldier.soldierId === soldierInfo?.soldierId
     : false;
+
+  const { isNew } = isLoggedIn ? await getIsNew(+session.user.userId) : {};
 
   return (
     <SidebarProvider defaultOpen={false} className="flex-col">
@@ -42,10 +44,10 @@ export default async function CabinetPage({ params }: Props) {
         <CabinetHeader isMyCabinet={isMyCabinet} soldierInfo={soldierInfo} />
         <FriendsList soldierId={+soldierId} />
         <StatusMessage isMyCabinet={isMyCabinet} soldierInfo={soldierInfo} />
-        <Cabinet
-          isMyCabinet={isMyCabinet}
+        <CabinetPagenation
           userId={+soldierInfo.userId}
           soldierId={+soldierId}
+          currentPage={+currentPage}
         />
         {!isMyCabinet && (
           <LetterMoneyButton
