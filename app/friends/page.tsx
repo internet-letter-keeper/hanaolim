1914a69874382f883b506584ca34c825a9ebe220
ryd10227 <ryd10227@gmail.com>
2025-06-30 01:29:48 +0900

    Refactor:  non null assertion 제거

--- app/friends/page.tsx
+++ app/friends/page.tsx
@@ -11,10 +11,10 @@ export default async function FriendsPage() {
 
   const { success, message, data } = await getFriendsList(userId);
 
-  if (!success) {
+  if (!success || !data) {
     throw new Error(message);
   }
-  const friends = data!;
+
   return (
     <div className="flex flex-col min-h-full">
       <BasicHeader title="친구 관리" />
@@ -22,13 +22,13 @@ export default async function FriendsPage() {
       {/* 친구 수와 친구 목록 구분선 */}
       <div className="flex flex-col bg-white-fff px-7 -mx-4 pt-4">
         <Txt size={11} weight="cm" align="left">
-          총 {friends.length}명
+          총 {data.length}명
         </Txt>
         <div className="border-b border-gray-400 px-40 h-2" />
       </div>
       {/* 친구 관리 목록 */}
       <div className="flex-1 flex flex-col bg-white-fff -mx-4 -mb-4 pb-8">
-        <FriendManageList friends={friends} />
+        <FriendManageList friends={data} />
       </div>
     </div>
   );
